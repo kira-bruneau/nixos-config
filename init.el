dc152061b5e89f45e2d51a3843e40c4173d7d933
Kira Bruneau <kira.bruneau@gmail.com>
2015-05-31 21:43:50 -0400

    A blob of changes that shouldn't be one commit

--- init.el
+++ init.el
@@ -1,19 +1,17 @@
+;; Model this: https://github.com/1ambda/emacs-linux
+
 ;; == Initialization == ;;
 (add-to-list 'load-path "~/.emacs.d/lisp/")
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(package-initialize)
-
 (setq package-list
-
-      '(;; 2048-game
+      '(adaptive-wrap
         auctex
-        adaptive-wrap
         buffer-move
         company
-        ;; company-irony
-       	ctags
+        company-quickhelp
+        company-tern
         ;; diminish
         drag-stuff
         dtrt-indent
@@ -23,15 +21,17 @@
         flx-ido
         flx-isearch
         flycheck
-		;; ggtags
+        ggtags
         ido-ubiquitous
+        ido-vertical-mode
+        js2-mode
         latex-preview-pane
         linum-off
         lua-mode
-        ;; irony
         magit
         monokai-theme
         multiple-cursors
+        php-mode
         pkgbuild-mode
         powerline
         projectile
@@ -39,13 +39,16 @@
         slime
         smartparens
         smex
-        ;; smooth-scrolling
         sublimity
-		sql-indent
+        tern
+        sql-indent
         undo-tree
         visual-regexp
         visual-regexp-steroids
-        web-mode))
+        web-mode
+        whitespace-cleanup-mode))
+
+(package-initialize)
 
 ;; Refresh the archive
 (unless package-archive-contents
@@ -62,8 +65,9 @@
 (global-undo-tree-mode t)
 (global-set-key (kbd "M-?") 'undo-tree-visualize)
 
-;; Project Management
+;; Project management
 (projectile-global-mode)
+(setq projectile-indexing-method 'alien)
 
 ;; Recent files
 (recentf-mode 1)
@@ -74,37 +78,45 @@
 (global-set-key (kbd "M-F") 'sp-forward-symbol)
 (global-set-key (kbd "M-B") 'sp-backward-symbol)
 
-;; Fuzzy match almost anything with ido
+;; Integrated git
+(setq magit-last-seen-setup-instructions "1.4.0")
+(global-set-key (kbd "C-c C-v") 'magit-status)
+
+;; Fuzzy find everywhere with ido
 (ido-mode 1)
+(ido-vertical-mode 1)
 (ido-everywhere 1)
 (ido-ubiquitous-mode t)
-(setq org-completion-use-ido t)
-(setq magit-completing-read-function 'magit-ido-completing-read)
 (setq ido-enable-flex-matching t)
 (setq ido-create-new-buffer 'always)
-(setq ido-use-virtual-buffers nil)
+(setq ido-use-virtual-buffers t)
 
-;; Fuzzy match commands with smex
+;; Smarter flex matching for ido
+(require 'flx-ido)
+(flx-ido-mode 1)
+(setq ido-use-faces nil)
+(setq flx-ido-use-faces t)
+
+;; Fuzzy isearch using flx
+(global-set-key (kbd "C-M-s") #'flx-isearch-forward)
+(global-set-key (kbd "C-M-r") #'flx-isearch-backward)
+
+;; Ido for M-x commands
 (smex-initialize)
 (global-set-key (kbd "M-x") 'smex)
 (global-set-key (kbd "M-X") 'smex-major-mode-commands)
 
-;; Super awesome (but intensive) flex matching for ido
-(require 'flx-ido)
-(flx-ido-mode 1)
-;; (setq gc-cons-threshold 20000000) ;; This is speeds up flx-ido-mode by doing GC less often
-(setq ido-use-faces nil)
-(setq flx-ido-use-faces t)
-
-;; Super awesome flex matching isearch
-(global-set-key (kbd "C-M-s") #'flx-isearch-forward)
-(global-set-key (kbd "C-M-r") #'flx-isearch-backward)
+;; ggtags
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode 'php-mode)
+              (ggtags-mode 1))))
 
 ;; Auto-completion
-;; (add-hook 'after-init-hook 'global-company-mode)
-;; (add-to-list 'company-backends 'company-math-symbols-unicode)
-;; (eval-after-load 'company
-;;   '(add-to-list 'company-backends 'company-irony))
+(company-quickhelp-mode 1)
+(setq company-idle-delay 0.3)
+(setq company-minimum-prefix-length 1)
+(add-hook 'after-init-hook 'global-company-mode)
 
 ;; Snippets
 ;; (yas-global-mode 1)
@@ -130,76 +142,45 @@
 (global-set-key (kbd "C-^") 'mc/mark-all-like-this)
 (global-set-key (kbd "C-'") 'mc-hide-unmatched-lines-mode)
 
-;; ;; Ace-jump
-;; (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
-;; (global-set-key (kbd "C-c C-a") 'ace-window)
-;; (setq aw-keys '(?a ?s ?d ?f ?g ?h ?j ?k ?l))
-
 ;; Visual regexp
 (define-key global-map (kbd "C-c r") 'vr/replace)
 (define-key global-map (kbd "C-c q") 'vr/query-replace)
 (define-key global-map (kbd "C-c m") 'vr/mc-mark)
-(define-key esc-map (kbd "C-r") 'vr/isearch-backward) ;; C-M-r
-(define-key esc-map (kbd "C-s") 'vr/isearch-forward) ;; C-M-s
+;; (define-key esc-map (kbd "M-r") 'vr/isearch-backward) ;; C-M-r
+;; (define-key esc-map (kbd "M-s") 'vr/isearch-forward) ;; C-M-s
 
 ;; Buffer move
-(global-set-key (kbd "<C-S-up>")     'buf-move-up)
-(global-set-key (kbd "<C-S-down>")   'buf-move-down)
-(global-set-key (kbd "<C-S-left>")   'buf-move-left)
-(global-set-key (kbd "<C-S-right>")  'buf-move-right)
+(global-set-key (kbd "<C-S-up>") 'buf-move-up)
+(global-set-key (kbd "<C-S-down>") 'buf-move-down)
+(global-set-key (kbd "<C-S-left>") 'buf-move-left)
+(global-set-key (kbd "<C-S-right>") 'buf-move-right)
 
 ;; C/C++
-;; (add-hook 'c++-mode-hook 'irony-mode)
-;; (add-hook 'c-mode-hook 'irony-mode)
-;; (add-hook 'objc-mode-hook 'irony-mode)
-
-;; ;; replace the `completion-at-point' and `complete-symbol' bindings in
-;; ;; irony-mode's buffers by irony-mode's function
-;; (defun my-irony-mode-hook ()
-;;   (define-key irony-mode-map [remap completion-at-point]
-;;     'irony-completion-at-point-async)
-;;   (define-key irony-mode-map [remap complete-symbol]
-;;     'irony-completion-at-point-async))
-;; (add-hook 'irony-mode-hook 'my-irony-mode-hook)
-;; (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
-
 (setq gdb-many-windows t)
 
-;; Web-mode
-(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
-(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+;; js2-mode
+;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+;; (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 
-;; SQL
+;; tern
+(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-tern))
+
+;; sql
 (eval-after-load "sql"
   '(load-library "sql-indent"))
 
-;; Elisp
+;; elisp
 (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
   (add-hook hook 'turn-on-elisp-slime-nav-mode))
 
-;; Lisp
-;; (load (expand-file-name "~/quicklisp/slime-helper.el"))
+;; lisp
+(load (expand-file-name "~/quicklisp/slime-helper.el"))
 (setq inferior-lisp-program "sbcl")
 (setq slime-contribs '(slime-fancy))
 (require 'slime)
 (slime-setup)
-
-;; Eval and replace any lisp expresison
-;; Source: https://emacs.wordpress.com/2007/01/17/eval-and-replace-anywhere/
-(defun fc-eval-and-replace ()
-  "Replace the preceding sexp with its value."
-  (interactive)
-  (backward-kill-sexp)
-  (prin1 (eval (read (current-kill 0)))
-         (current-buffer)))
-
-(global-set-key (kbd "C-c e") 'fc-eval-and-replace)
 
 ;; Org-mode
 (global-set-key (kbd "C-c l") 'org-store-link)
@@ -210,37 +191,65 @@
 ;; (latex-preview-pane-enable)
 
 ;; Fix annoyances
-(defalias 'yes-or-no-p 'y-or-n-p)
-(setq save-interprogram-paste-before-kill t)
-(setq confirm-nonexistent-file-or-buffer nil)
-(global-set-key (kbd "C-c C-k") 'kill-this-buffer)
+(defalias 'yes-or-no-p 'y-or-n-p) ;; Simplify all yes/no to y/n
+(setq save-interprogram-paste-before-kill t) ;; Push clipboard to the kill ring
+(setq confirm-nonexistent-file-or-buffer nil) ;; Don't ask to create a new file
+(global-set-key (kbd "C-c C-k") 'kill-this-buffer) ;; Don't prompt to kill a buffer
+;;(setq electric-indent-chars (remq ?\n electric-indent-chars)) ;; Don't indent on newline
 
-;; Centralized backups:
-;; Source http://www.emacswiki.org/emacs/BackupDirectory
-;; (setq
-;;  backup-by-copying t      ; don't clobber symlinks
-;;  backup-directory-alist
-;;  '(("." . "~/.saves"))    ; don't litter my fs tree
-;;  delete-old-versions t
-;;  kept-new-versions 6
-;;  kept-old-versions 2
-;;  version-control t)       ; use versioned backups
-
-;; Misc convenience settings
+;; Misc
 (winner-mode t)
+(recentf-mode t)
+(dtrt-indent-mode t)
+(setq-default indent-tabs-mode nil)
+(global-visual-line-mode t)
 (delete-selection-mode t)
 (setq shift-select-mode nil)
 (windmove-default-keybindings)
 (drag-stuff-global-mode t)
+(global-whitespace-cleanup-mode t)
 
-;; (visual-line-mode)
-;; (global-visual-line-mode t)
-
-(add-hook 'before-save-hook 'delete-trailing-whitespace)
-
-;; Stuff I don't really use
 (global-set-key (kbd "C-x C--") 'goto-last-change)
 (global-set-key (kbd "C-=") 'er/expand-region)
+
+;; == Graphical settings == ;;
+(load-theme 'monokai t)
+
+;; GUI cleanup
+(scroll-bar-mode -1)
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(tooltip-mode -1)
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
+;; Line numbering on most major modes
+;; (global-linum-mode t)
+;; (require 'linum-off)
+;; (add-to-list 'linum-disabled-modes-list 'doc-view-mode)
+
+;; Sublime-like smooth scrolling
+;; (require 'sublimity-scroll) ;; This can be annoying on eshell, figure out how to fix
+;; (sublimity-mode 1)
+
+;; Powerline mode
+(powerline-default-theme)
+
+;; Diminish certain modeline modes
+;; (diminish 'smartparens-mode "")
+;; (diminish 'undo-tree-mode "")
+;; (diminish 'elisp-slime-nav-mode "")
+;; (diminish 'projectile-mode "")
+
+;; Misc
+(setq-default tab-width 4)
+(setq column-number-mode t)
+(show-paren-mode t)
+(setq-default word-wrap t)
+(add-hook 'visual-line-mode-hook 'adaptive-wrap-prefix-mode)
+;;(setq doc-view-resolution 200) ; Note: this makes latex-preview-pane-mode crash randomly
+
+;; Extra ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Auto-indent yanked lines (try auto-indent instead)
 ;; Source: http://www.emacswiki.org/emacs/AutoIndentation
@@ -257,42 +266,27 @@
                 (let ((mark-even-if-inactive transient-mark-mode))
                   (indent-region (region-beginning) (region-end) nil))))))
 
-;; == Graphical settings == ;;
-(load-theme 'monokai t)
+;; Centralized backups:
+;; Source http://www.emacswiki.org/emacs/BackupDirectory
+;; (setq
+;;  backup-by-copying t      ; don't clobber symlinks
+;;  backup-directory-alist
+;;  '(("." . "~/.saves"))    ; don't litter my fs tree
+;;  delete-old-versions t
+;;  kept-new-versions 6
+;;  kept-old-versions 2
+;;  version-control t)       ; use versioned backups
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 98 :width normal))))
- '(vertical-border ((t (:foreground "#1f1f1b")))))
+;; Eval and replace any lisp expresison
+;; Source: https://emacs.wordpress.com/2007/01/17/eval-and-replace-anywhere/
+(defun fc-eval-and-replace ()
+  "Replace the preceding sexp with its value."
+  (interactive)
+  (backward-kill-sexp)
+  (prin1 (eval (read (current-kill 0)))
+         (current-buffer)))
 
-;; GUI cleanup
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
-(tooltip-mode -1)
-(setq inhibit-startup-message t)
-(setq initial-scratch-message nil)
-
-;; Line numbering on most major modes
-(global-linum-mode t)
-(require 'linum-off)
-(add-to-list 'linum-disabled-modes-list 'doc-view-mode)
-
-;; Sublime-like smooth scrolling
-(require 'sublimity-scroll) ;; This can be annoying on eshell, figure out how to fix
-(sublimity-mode 1)
-
-;; Powerline mode
-(powerline-default-theme)
-
-;; Diminish certain modeline modes
-;; (diminish 'smartparens-mode "")
-;; (diminish 'undo-tree-mode "")
-;; (diminish 'elisp-slime-nav-mode "")
-;; (diminish 'projectile-mode "")
+(global-set-key (kbd "C-c e") 'fc-eval-and-replace)
 
 (defun sudo-edit ()
   "Re-open a file with elevated privileges"
@@ -303,12 +297,17 @@
    (t
     (message "current buffer is already writeable"))))
 
-;; Misc
-;; (dtrt-indent-mode t)
-(setq indent-tabs-mode nil)
-(setq-default tab-width 4)
-(setq column-number-mode t)
-(show-paren-mode t)
-(setq-default word-wrap t)
-(adaptive-wrap-prefix-mode) ;; Indent on wrap
-;;(setq doc-view-resolution 200) ; Note: this makes latex-preview-pane-mode crash randomly
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Monaco" :foundry "unknown" :slant normal :weight normal :height 98 :width normal))))
+ '(vertical-border ((t (:foreground "#1f1f1b")))))
+
+;; (custom-set-variables
+;;  ;; custom-set-variables was added by Custom.
+;;  ;; If you edit it by hand, you could mess it up, so be careful.
+;;  ;; Your init file should contain only one such instance.
+;;  ;; If there is more than one, they won't work right.
+;;  '(cursor-type (quote (bar . 1))))
