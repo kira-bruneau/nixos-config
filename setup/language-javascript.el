3bed2e4d42a4c6d987e0b0972154f04807bc7c2a
Kira Bruneau <kira.bruneau@gmail.com>
2015-06-19 18:51:21 -0400

    Enabled js2-mode by default

--- setup/language-javascript.el
+++ setup/language-javascript.el
@@ -1,9 +1,9 @@
 ;; js2-mode
-;; (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-;; (add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
 
 ;; tern
-(add-hook 'js-mode-hook (lambda () (tern-mode t)))
+(add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-tern))
 
