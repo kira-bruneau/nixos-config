488d63fecde4bcfb59581355e196adced0580966
Kira Bruneau <kira.bruneau@gmail.com>
2015-07-02 00:31:09 -0400

    Added json-mode and disabled js2-mode warnings

--- setup/language-javascript.el
+++ setup/language-javascript.el
@@ -1,15 +1,20 @@
 (package-require
  '(company-tern
    js2-mode
+   json-mode ;; I prefer this over js2-mode for json files
    tern))
 
 ;; js2-mode
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
-(add-to-list 'auto-mode-alist '("\\.json$" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.json$" . json-mode))
+(setq js2-mode-show-strict-warnings nil)
+
 
 ;; tern
 (add-hook 'js2-mode-hook (lambda () (tern-mode t)))
 (eval-after-load 'company
   '(add-to-list 'company-backends 'company-tern))
+
+(add-to-list 'auto-mode-alist '("\\.tern-project$" . json-mode))
 
 (provide 'language-javascript)
