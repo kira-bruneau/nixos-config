85587fad82c7d0f37843490322487b3e92797261
Kira Bruneau <kira.bruneau@gmail.com>
2015-07-21 11:23:25 -0400

    Commented out old broken key mapping

--- setup/language-lisp.el
+++ setup/language-lisp.el
@@ -13,14 +13,15 @@
 
 (global-set-key (kbd "C-c e") 'fc-eval-and-replace)
 
-;; elisp
+;; Emacs lisp
 (dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
   (add-hook hook 'turn-on-elisp-slime-nav-mode))
 
 ;; Shorter key binding for documentation
-(define-key elisp-slime-nav-mode-map (kbd "C-c C-d")
-  'elisp-slime-nav-describe-elisp-hing-at-point)
+;; (define-key elisp-slime-nav-mode-map (kbd "C-c C-d")
+;;   'elisp-slime-nav-describe-elisp-hing-at-point)
 
+;; Common lisp
 ;; (load (expand-file-name "~/quicklisp/slime-helper.el"))
 ;; (setq inferior-lisp-program "sbcl")
 ;; (setq slime-contribs '(slime-fancy))
