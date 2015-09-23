83043c751d7c9459c44d6bf4795e694e1a61d889
Kira Bruneau <kira.bruneau@gmail.com>
2015-09-23 00:33:33 -0400

    Reorder sections in magit-status

--- setup/setup-git.el
+++ setup/setup-git.el
@@ -2,18 +2,32 @@
  '(git-gutter-fringe
    gitconfig-mode
    gitignore-mode
-   ;; magit-filenotify
    magit))
 
 (require-binary
  '(git))
 
+;; Magit
 (setq magit-last-seen-setup-instructions "1.4.0")
 (global-set-key (kbd "C-c C-v") 'magit-status)
-(global-git-gutter-mode t)
 
-;; (add-hook 'magit-status-mode-hook
-;;           (lambda ()
-;;             (magit-filenotify-mode t)))
+;; Set custom order for magit-status
+(setq magit-status-sections-hook
+      '(magit-insert-status-headers
+        magit-insert-merge-log
+        magit-insert-rebase-sequence
+        magit-insert-am-sequence
+        magit-insert-sequencer-sequence
+        magit-insert-bisect-output
+        magit-insert-bisect-rest
+        magit-insert-bisect-log
+        magit-insert-stashes
+        magit-insert-staged-changes
+        magit-insert-unstaged-changes
+        magit-insert-untracked-files
+        magit-insert-unpulled-commits
+        magit-insert-unpushed-commits))
+
+(global-git-gutter-mode t)
 
 (provide 'setup-git)
