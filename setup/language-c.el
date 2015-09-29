42f891473be9299ec20db7b3e5da4b7e9a3e3564
Kira Bruneau <kira.bruneau@gmail.com>
2015-09-28 20:47:51 -0400

    Handle include paths with flycheck

--- setup/language-c.el
+++ setup/language-c.el
@@ -3,4 +3,16 @@
 
 (setq gdb-many-windows t)
 
+(setq flycheck-clang-include-path (list "/usr/include"))
+
+(defun pkg-config-includes (extern-libs)
+  (mapcar
+   (lambda (include)
+     (replace-regexp-in-string "^-I" "" include))
+   (split-string
+    (shell-command-to-string
+     (concat
+      "pkg-config --cflags-only-I "
+      (mapconcat 'symbol-name extern-libs " "))))))
+
 (provide 'language-c)
