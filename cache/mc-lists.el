2769a7af85876cb3ff3f18a51c699735c0e93509
Kira Bruneau <kira.bruneau@gmail.com>
2015-09-29 22:11:32 -0400

    Added more multiple cursor completion items

--- cache/mc-lists.el
+++ cache/mc-lists.el
@@ -12,6 +12,7 @@
         c-electric-semi&comma
         c-electric-slash
         c-electric-star
+        comment-dwim
         dired-flag-file-deletion
         dired-mark
         dired-next-line
