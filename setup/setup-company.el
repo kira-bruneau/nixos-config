88a1ea69381a5194ec27bc5683d1b29168c6256e
Kira Bruneau <kira.bruneau@gmail.com>
2015-10-02 01:34:25 -0400

    Disable company in eshell-mode

--- setup/setup-company.el
+++ setup/setup-company.el
@@ -4,6 +4,7 @@
 ;; (company-quickhelp-mode 1)
 (setq company-idle-delay 0.1)
 (setq company-minimum-prefix-length 1)
+(setq company-global-modes '(not eshell-mode))
 (add-hook 'after-init-hook 'global-company-mode)
 
 (provide 'setup-company)
