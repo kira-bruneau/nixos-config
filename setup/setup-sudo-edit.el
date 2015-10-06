9232682073b777410dbe916a177a88de700d6f55
Kira Bruneau <kira.bruneau@gmail.com>
2015-10-06 17:01:45 -0400

    sudo-edit is now usable

--- setup/setup-sudo-edit.el
+++ setup/setup-sudo-edit.el
@@ -1,18 +1,23 @@
 ;; Best implementation so far: http://www.emacswiki.org/emacs/sudo-save.el
 
 (defun sudo-edit ()
-  "Re-open a file with elevated privileges"
+  "Re-open the current buffer with elevated privileges"
   (interactive)
-  (cond
-   ((not (file-writable-p buffer-file-name))
-    (write-file (format "/sudo::%s" buffer-file-name)))
-   (t
-    (message "current buffer is already writeable"))))
+  (let ((old-buffer (current-buffer)))
+    (find-file (concat "/sudo::" (buffer-file-name)))
+    (kill-buffer old-buffer)))
 
+;; Try to open unwriteable files with sudo
+;; TODO I should check the user ids
+;; (eg. This file has been made unwritable by root, edit with sudo?)
 (add-hook 'find-file-hook
           (lambda ()
             (unless (file-writable-p (buffer-file-name))
               (when (y-or-n-p "This file is unwriteable, open with sudo?")
-                (print (concat "/sudo::" buffer-file-name))))))
+                (sudo-edit)))))
+
+;; TODO Try to open unreadable files with sudo
+
+;; TODO Try to open unreadable directories with sudo
 
 (provide 'setup-sudo-edit)
