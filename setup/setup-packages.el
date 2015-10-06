910e0e212a53b9f3491cded4d80ca539a6679b75
Kira Bruneau <kira.bruneau@gmail.com>
2015-10-06 16:22:02 -0400

    Ignore binary packages for now

--- setup/setup-packages.el
+++ setup/setup-packages.el
@@ -19,9 +19,10 @@
       (package-install package))))
 
 (defun require-binary (externs)
-  (message (concat
-            "sudo pacman -S --needed "
-            (mapconcat 'symbol-name externs " "))))
+  ;; (message (concat
+  ;;           "sudo pacman -S --needed "
+  ;;           (mapconcat 'symbol-name externs " ")))
+  )
 
 (require-package package-list)
 
