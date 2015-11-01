449e057b3d29b3f0346d16df1512a406e4a339dd
Kira Bruneau <kira.bruneau@gmail.com>
2015-11-01 03:45:00 -0500

    Simplified setting up c project dependencies

--- setup/language-c.el
+++ setup/language-c.el
@@ -1,11 +1,14 @@
 (require-package
  '(company-c-headers))
 
+(eval-after-load 'company
+  '(add-to-list 'company-backends 'company-c-headers))
+
 (setq gdb-many-windows t)
 
-(setq flycheck-clang-include-path (list "/usr/include"))
-
+;; TODO: Handle errors (pkg-config not installed, invalid library, etc...)
 (defun pkg-config-includes (extern-libs)
+  "Obtain a list of include directories from pkg-config"
   (mapcar
    (lambda (include)
      (replace-regexp-in-string "^-I" "" include))
@@ -14,5 +17,49 @@
      (concat
       "pkg-config --cflags-only-I "
       (mapconcat 'identity extern-libs " "))))))
+
+(defun cproject-string-list-p (obj)
+  "Determine if OBJ is a list of strings."
+  (and (listp obj) (-all? #'stringp obj)))
+
+(defcustom cproject-pkg-config nil
+  "A list of pkg-config libraries to include in your C project"
+  :type '(repeat string)
+  :safe #'cproject-string-list-p)
+
+(defcustom cproject-include-path nil
+  "A list of include directories to use in your C project"
+  :type '(repeat string)
+  :safe #'cproject-string-list-p)
+
+;; Read project configuration from .dir-locals.el
+(add-hook 'c-mode-hook 'cproject-configure-locals)
+(defun cproject-configure-locals ()
+  (flycheck-mode t)
+  (add-hook 'hack-local-variables-hook
+            (lambda ()
+              ;; Add include paths to flycheck
+              (setq-local flycheck-clang-include-path
+                          (append
+                           (when (boundp 'flycheck-clang-include-path)
+                             flycheck-clang-include-path)
+                           (when (boundp 'cproject-include-path)
+                             (mapcar
+                              (lambda (include)
+                                (expand-file-name include (locate-dominating-file default-directory ".dir-locals.el")))
+                              cproject-include-path))
+                           (when (boundp 'cproject-pkg-config)
+                             (pkg-config-includes cproject-pkg-config))))
+
+              ;; Add include paths to company
+              (setq-local company-clang-arguments
+                          (append
+                           (when (boundp 'company-clang-arguments)
+                             company-clang-arguments)
+                           (when (boundp 'cproject-pkg-config)
+                             (mapcar
+                              (lambda (include)
+                                (concat "-I" include))
+                              (pkg-config-includes cproject-pkg-config))))))))
 
 (provide 'language-c)
