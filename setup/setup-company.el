c2f4fce30d7424bf851ed16733bf55d0eeaec802
Kira Bruneau <kira.bruneau@gmail.com>
2015-11-02 00:43:54 -0500

    Automatic argument insertion is annoying

--- setup/setup-company.el
+++ setup/setup-company.el
@@ -5,6 +5,7 @@
 (setq company-idle-delay 0.1)
 (setq company-minimum-prefix-length 1)
 (setq company-global-modes '(not eshell-mode))
+(setq company-clang-insert-arguments nil)
 (add-hook 'after-init-hook 'global-company-mode)
 
 (provide 'setup-company)
