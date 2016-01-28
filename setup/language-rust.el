53f5d289004d71844220a4c95d8e80ba10bef966
Kira Bruneau <kira.bruneau@gmail.com>
2016-01-28 01:10:54 -0500

    Setup flycheck for rust

--- setup/language-rust.el
+++ setup/language-rust.el
@@ -10,8 +10,12 @@
 (setq racer-cmd "/home/kira/.cargo/bin/racer")
 (setq racer-rust-src-path "/home/kira/dev/public/rust/src/")
 
-(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'rust-mode-hook (lambda ()
+                            (racer-mode)
+                            (flycheck-mode)))
+
 (add-hook 'racer-mode-hook #'eldoc-mode)
 (add-hook 'racer-mode-hook #'company-mode)
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
 
 (provide 'language-rust)
--- setup/setup-linting.el
+++ setup/setup-linting.el
@@ -1,7 +1,4 @@
 (require-package
  '(flycheck))
 
-;; (global-flycheck-mode t)
-;; (setq flycheck-check-syntax-automatically '(mode-enabled save))
-
 (provide 'setup-linting)
