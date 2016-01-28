92977f91c1dfd487c43c1ba398ff5f39529c7b54
Kira Bruneau <kira.bruneau@gmail.com>
2016-01-28 13:07:45 -0500

    Do not downcase dabbrev completions

--- setup/setup-company.el
+++ setup/setup-company.el
@@ -6,4 +6,7 @@
 (setq company-clang-insert-arguments nil)
 (add-hook 'after-init-hook 'global-company-mode)
 
+(setq company-dabbrev-downcase nil)
+(setq company-dabbrev-ignore-case nil)
+
 (provide 'setup-company)
