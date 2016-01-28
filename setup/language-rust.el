75b36c0c25bed782a053fee01b4840c026fb60af
Kira Bruneau <kira.bruneau@gmail.com>
2016-01-27 19:26:55 -0500

    Use racer & company-racer for rust

--- setup/language-rust.el
+++ setup/language-rust.el
@@ -1,10 +1,17 @@
 (require-package
- '(company-racer
-   flycheck-rust
+ '(rust-mode
    racer
-   rust-mode))
+   company-racer
+   flycheck-rust))
 
 (require-binary
  '(racer))
+
+(setq racer-cmd "/home/kira/.cargo/bin/racer")
+(setq racer-rust-src-path "/home/kira/dev/public/rust/src/")
+
+(add-hook 'rust-mode-hook #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'racer-mode-hook #'company-mode)
 
 (provide 'language-rust)
