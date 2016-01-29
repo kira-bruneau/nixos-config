b563fd9544a30e910231d7a5fb4942c6faebc0eb
Kira Bruneau <kira.bruneau@gmail.com>
2016-01-29 11:13:34 -0500

    Use dedicated window for multi-term

--- setup/setup-multi-term.el
+++ setup/setup-multi-term.el
@@ -5,6 +5,7 @@
  '(zsh))
 
 (setq multi-term-program "/bin/zsh")
-(global-set-key (kbd "C-z") 'multi-term)
+(global-set-key (kbd "C-S-z") 'multi-term-dedicated-toggle)
+(setq multi-term-dedicated-select-after-open-p t)
 
 (provide 'setup-multi-term)
