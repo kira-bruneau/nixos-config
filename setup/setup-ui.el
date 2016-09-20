b63c3e90411de6db1ac762594670ac6fa414f1f5
Kira Bruneau <kira.bruneau@gmail.com>
2016-09-20 17:33:40 -0400

    Only apply custom border to monokai theme

--- setup/setup-ui.el
+++ setup/setup-ui.el
@@ -5,12 +5,8 @@
    linum-off))
 
 (load-theme 'monokai t)
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+(custom-theme-set-faces
+ 'monokai
  '(vertical-border ((t (:foreground "#1f1f1b")))))
 
 ;; Powerline mode
