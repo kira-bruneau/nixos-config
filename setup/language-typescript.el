1c991262653b81a19b72c516eef1434d7e515c51
Kira Bruneau <kira.bruneau@gmail.com>
2016-12-01 12:20:19 -0500

    Improve typescript editing

--- setup/language-typescript.el
+++ setup/language-typescript.el
@@ -2,6 +2,14 @@
  '(tide
    typescript-mode))
 
-(add-hook 'typescript-mode-hook 'tide-setup)
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode t)
+  (company-mode t)
+  (eldoc-mode t)
+  (tide-hl-identifier-mode t))
+
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
 
 (provide 'language-typescript)
