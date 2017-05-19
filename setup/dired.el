89ab8a749c9eef6598318d13274cecaadfdf3f57
Kira Bruneau <kira.bruneau@gmail.com>
2017-05-18 22:39:57 -0400

    Auto revert dired

--- setup/dired.el
+++ setup/dired.el
@@ -1,34 +1,34 @@
 (add-hook 'dired-mode-hook
           (lambda ()
-            (define-key dired-mode-map (kbd "E") 'xah-open-in-external-app)))
-
-(setq dired-auto-revert-buffer t)
+            (define-key dired-mode-map (kbd "E") 'xah-open-in-external-app)
+            (auto-revert-mode)))
 
 (defun xah-open-in-external-app ()
   "Open the current file or dired marked files in external app.
 The app is chosen from your OS's preference.
-
 URL `http://ergoemacs.org/emacs/emacs_dired_open_file_in_ext_apps.html'
-Version 2015-01-26"
+Version 2016-10-15"
   (interactive)
   (let* (
-         (ξfile-list
+         (-file-list
           (if (string-equal major-mode "dired-mode")
               (dired-get-marked-files)
             (list (buffer-file-name))))
-         (ξdo-it-p (if (<= (length ξfile-list) 5)
+         (-do-it-p (if (<= (length -file-list) 5)
                        t
                      (y-or-n-p "Open more than 5 files? "))))
-
-    (when ξdo-it-p
+    (when -do-it-p
       (cond
        ((string-equal system-type "windows-nt")
         (mapc
-         (lambda (fPath)
-           (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" fPath t t))) ξfile-list))
+         (lambda (-fpath)
+           (w32-shell-execute "open" (replace-regexp-in-string "/" "\\" -fpath t t))) -file-list))
        ((string-equal system-type "darwin")
         (mapc
-         (lambda (fPath) (shell-command (format "open \"%s\"" fPath)))  ξfile-list))
+         (lambda (-fpath)
+           (shell-command
+            (concat "open " (shell-quote-argument -fpath))))  -file-list))
        ((string-equal system-type "gnu/linux")
         (mapc
-         (lambda (fPath) (let ((process-connection-type nil)) (start-process "" nil "xdg-open" fPath))) ξfile-list))))))
+         (lambda (-fpath) (let ((process-connection-type nil))
+                            (start-process "" nil "xdg-open" -fpath))) -file-list))))))
