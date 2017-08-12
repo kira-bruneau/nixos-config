414ac96cf15812b4f7dc2df9d5173948cf397209
Kira Bruneau <kira.bruneau@gmail.com>
2017-08-12 15:16:30 -0400

    Moved smooth-scroll.el back into ui.el since it's not a minor mode

--- setup/ui.el
+++ setup/ui.el
@@ -28,6 +28,13 @@
 (global-set-key (kbd "C-c v") 'visual-line-mode)
 (diminish 'visual-line-mode "↩")
 
+;; Scroll one line at a time (less "jumpy" than defaults)
+;; Source: http://www.emacswiki.org/emacs/SmoothScrolling
+(setq mouse-wheel-scroll-amount '(3 ((shift) . 1))) ;; one line at a time
+(setq mouse-wheel-progressive-speed nil) ;; don't accelerate scrolling
+(setq mouse-wheel-follow-mouse 't) ;; scroll window under mouse
+;; (setq scroll-step 1) ;; keyboard scroll one line at a time
+
 ;; Misc
 (setq-default tab-width 4)
 (setq column-number-mode t)
