b250aa1e0bf60dcd80dfa28326be934c2ead77db
Kira Bruneau <kira.bruneau@gmail.com>
2017-08-12 17:07:33 -0400

    Added web beautify

--- setup/major-modes/php.el
+++ setup/major-modes/php.el
@@ -1,5 +1,4 @@
 (straight-use-package 'php-mode)
-(straight-use-package 'web-mode)
 
 (add-hook 'php-mode-hook
           (lambda ()
---
+++ setup/major-modes/web.el
@@ -0,0 +1,19 @@
+(straight-use-package 'web-beautify)
+(straight-use-package 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+
+(eval-after-load 'js2-mode
+  '(define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
+
+(eval-after-load 'json-mode
+  '(define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
+
+(eval-after-load 'sgml-mode
+  '(define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
+
+(eval-after-load 'web-mode
+  '(define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
+
+(eval-after-load 'css-mode
+  '(define-key css-mode-map (kbd "C-c b") 'web-beautify-css))
