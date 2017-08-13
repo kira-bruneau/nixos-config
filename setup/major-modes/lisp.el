3c8965a9f7405a3f5b84e47fc7375be0cb373bfd
Kira Bruneau <kira.bruneau@gmail.com>
2017-08-12 22:00:00 -0400

    Got rid of elisp-slime-nav since emacs 25.1 introduced xref

--- setup/major-modes/lisp.el
+++ setup/major-modes/lisp.el
@@ -1,17 +1,6 @@
-(straight-use-package 'elisp-slime-nav) ;; I think this is now built into emacs
 (straight-use-package 'slime)
 
-(dolist (hook '(emacs-lisp-mode-hook ielm-mode-hook))
-  (add-hook hook 'turn-on-elisp-slime-nav-mode))
-
-(with-eval-after-load "elisp-slime-nav"
-  (diminish 'elisp-slime-nav-mode " ◎"))
-
 (global-set-key (kbd "C-c e") 'fc-eval-and-replace)
-
-;; Shorter key binding for documentation
-;; (define-key elisp-slime-nav-mode-map (kbd "C-c C-d")
-;;   'elisp-slime-nav-describe-elisp-hing-at-point)
 
 ;; Eval and replace any lisp expresison
 ;; Source: https://emacs.wordpress.com/2007/01/17/eval-and-replace-anywhere/
