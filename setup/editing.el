80f08e6f9542bfde0fb401b008a61172ae39a62f
Kira Bruneau <kira.bruneau@gmail.com>
2017-08-13 00:59:41 -0400

    Disable file local variables
    I feel like embedding emacs specific metadata into any file is bad
    practice, especially if the file will be shared.
    
    Emacs should be able to determine the major mode using standard
    methods (file extension, magic number, etc...) and any specific mode
    customizations should apply on the scope of a project, not a file.

--- setup/editing.el
+++ setup/editing.el
@@ -1,3 +1,8 @@
 (delete-selection-mode t)
 (setq shift-select-mode nil)
 (setq-default indent-tabs-mode nil)
+
+;; Turn off file variables
+;; Source: https://www.emacswiki.org/emacs/FileLocalVariables#toc2
+(setq enable-local-variables nil
+      enable-local-eval nil)
