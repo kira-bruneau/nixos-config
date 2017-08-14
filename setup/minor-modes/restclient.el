a0281d92c187ff947b90dc50d28511bb49ee7a00
Kira Bruneau <kira.bruneau@gmail.com>
2017-08-13 21:02:06 -0400

    Fix setting company backend for restclient

--- setup/minor-modes/restclient.el
+++ setup/minor-modes/restclient.el
@@ -1,5 +1,5 @@
 (straight-use-package 'restclient)
 (straight-use-package 'company-restclient)
 
-(with-eval-after-load 'restclient
+(with-eval-after-load 'company
   (add-to-list 'company-backends 'company-restclient))
