be291a13129ef90cfbf4487f8f383a599becfa32
Kira Bruneau <kira.bruneau@gmail.com>
2018-03-08 02:18:49 -0500

    Added more avy keys

--- setup/minor-modes/avy.el
+++ setup/minor-modes/avy.el
@@ -1,8 +1,12 @@
 (straight-use-package 'avy)
 
-(setq avy-keys '(?a ?r ?s ?t ?d ?h ?n ?e ?i ?o))
+;; Colemak optimized  keys
+(setq avy-keys '(?a ?r ?s ?t ?d ?h ?n ?e ?i ?o ?w ?f ?p ?l ?u ?y))
 
 (global-set-key (kbd "C-:") 'avy-goto-char)
 (global-set-key (kbd "C-'") 'avy-goto-char-2)
+
+(global-set-key (kbd "M-g w") 'avy-goto-word-1)
+(global-set-key (kbd "M-g e") 'avy-goto-word-0)
 
 (avy-setup-default)
