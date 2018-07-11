793cc4880874c1ca2735658fda8751086faf8d50
Kira Bruneau <kira.bruneau@gmail.com>
2018-07-11 01:20:49 -0400

    Removed helm

--- setup/minor-modes/helm.el
+++
@@ -1,7 +0,0 @@
-;; NOTE: I never actually use this, is it worth keeping?
-(straight-use-package 'helm)
-(straight-use-package 'helm-dash)
-
-(require 'helm-config)
-
-(global-set-key (kbd "s-.") 'helm-dash-at-point)
