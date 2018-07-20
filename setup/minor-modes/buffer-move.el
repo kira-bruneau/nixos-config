aaadd7ca486dd58a53f76370d6b5d25e4bbf5457
Kira Bruneau <kira.bruneau@gmail.com>
2018-07-19 21:16:57 -0400

    Configured bindings for moving window focus and moving windows

--- setup/minor-modes/buffer-move.el
+++ setup/minor-modes/buffer-move.el
@@ -1,7 +1,7 @@
 (use-package buffer-move
   :straight t
   ;; Mirror i3 window move commands (vim-like + colemak layout)
-  :bind (("C-S-n" . buf-move-left)
-         ("C-S-e" . buf-move-down)
-         ("C-S-i" . buf-move-up)
-         ("C-S-o" . buf-move-right)))
+  :bind (("C-M-S-n" . buf-move-left)
+         ("C-M-S-e" . buf-move-down)
+         ("C-M-S-i" . buf-move-up)
+         ("C-M-S-o" . buf-move-right)))
--- setup/minor-modes/windmove.el
+++ setup/minor-modes/windmove.el
@@ -1,1 +1,6 @@
-(windmove-default-keybindings)
+(use-package windmove
+  ;; Mirror i3 window move commands (vim-like + colemak layout)
+  :bind (("C-S-n" . windmove-left)
+         ("C-S-e" . windmove-down)
+         ("C-S-i" . windmove-up)
+         ("C-S-o" . windmove-right)))
