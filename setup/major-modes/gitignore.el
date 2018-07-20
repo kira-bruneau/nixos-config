c85fc5658542246c37d2cfe638a0abf99920d296
Kira Bruneau <kira.bruneau@gmail.com>
2018-07-20 00:10:48 -0400

    Added global bindings for common git commands

---
+++ setup/major-modes/gitignore.el
@@ -0,0 +1,2 @@
+(use-package gitignore-mode
+  :straight t)
--- setup/minor-modes/git.el
+++ setup/minor-modes/git.el
@@ -2,14 +2,18 @@
   :straight (magit :type git :host github :repo "MetaDark/magit"
                    :upstream (:host github :repo "magit/magit"))
   :ensure-system-package git
+  :bind ("C-c C-b" . magit-blame)
   :config
   (setq magit-save-repository-buffers nil))
 
 (use-package git-timemachine
-  :straight t)
+  :straight t
+  :bind ("C-c C-t" . git-timemachine))
 
-(use-package gitconfig-mode
-  :straight t)
+(use-package browse-at-remote
+  :straight t
+  :bind ("C-c o" . browse-at-remote))
 
-(use-package gitignore-mode
-  :straight t)
+
+
+
