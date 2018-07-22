20b57182885ba44ea1c16efc54350324037d0baf
Kira Bruneau <kira.bruneau@gmail.com>
2018-07-22 12:55:38 -0400

    Added binding to start latex-preview-mode

--- setup/major-modes/latex.el
+++ setup/major-modes/latex.el
@@ -1,6 +1,10 @@
 ;; (use-package tex
 ;;   :ensure auctex)
 
+(use-package tex-mode
+  :bind (:map latex-mode-map
+              ("M-p" . latex-preview-pane-mode)))
+
 (use-package latex-preview-pane
   :straight t
   :defer t)
