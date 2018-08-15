9672e00238dee76e29b7f8ddf941ae5c100cd757
Kira Bruneau <kira.bruneau@gmail.com>
2018-08-14 21:56:58 -0400

    Added aspell dictionaries for emacs

--- package/emacs.nix
+++ package/emacs.nix
@@ -4,6 +4,9 @@
   # My emacs configuration will automatically install system packages
   # using system-packages.el.
   environment.systemPackages = with pkgs; [
+    aspellDicts.en
+    aspellDicts.en-computers
+    aspellDicts.en-science
     emacs
     git # required to clone system-packages.el
   ];
