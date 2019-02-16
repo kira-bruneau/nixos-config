1f9f0a735a88eba78e74db006cba1f18b97effc4
Kira Bruneau <kira.bruneau@gmail.com>
2019-02-16 16:03:11 -0500

    Use ivy--regex for external searches

--- setup/minor-modes/counsel.el
+++ setup/minor-modes/counsel.el
@@ -11,6 +11,6 @@
   (counsel-mode)
 
   :config
-  (add-to-list 'ivy-re-builders-alist '(counsel-ag-function . ivy--regex-plus))
-  (add-to-list 'ivy-re-builders-alist '(counsel-fzf-function . ivy--regex-plus))
+  (add-to-list 'ivy-re-builders-alist '(counsel-ag-function . ivy--regex))
+  (add-to-list 'ivy-re-builders-alist '(counsel-fzf-function . ivy--regex))
   (add-to-list 'ivy-sort-functions-alist '(counsel-fzf-function . nil)))
