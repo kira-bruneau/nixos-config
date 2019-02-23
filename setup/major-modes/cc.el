8f0fb88e5e098e0ad50feacf3290f87049a8b315
Kira Bruneau <kira.bruneau@gmail.com>
2019-02-23 08:34:17 -0500

    Disable installing ccls until it has better packaging support

--- setup/major-modes/cc.el
+++ setup/major-modes/cc.el
@@ -1,5 +1,5 @@
 (use-package ccls
   :straight t
-  :ensure-system-package (ccls . "yay ccls")
+  ;; :ensure-system-package ccls
   :hook (((c-mode c++-mode objc-mode) .
           (lambda () (require 'ccls) (lsp)))))
