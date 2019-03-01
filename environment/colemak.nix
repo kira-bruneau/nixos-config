43dbab9308842751a184865950cdb903af3702b7
Kira Bruneau <kira.bruneau@gmail.com>
2019-03-01 12:55:20 -0500

    Generate console keymap from xkb
    This fixes the capslock backspace issue in emacs

--- environment/colemak.nix
+++ environment/colemak.nix
@@ -1,13 +1,11 @@
 { config, pkgs, ... }:
 
 {
-  i18n = {
-    consoleKeyMap = "colemak";
-  };
-
   services.xserver = {
     layout = "us,us";
     xkbVariant = "colemak,";
     xkbOptions = "grp:win_space_toggle";
   };
+
+  i18n.consoleUseXkbConfig = true;
 }
