634ec23edc44083248a89d2be2504957754decc7
Kira Bruneau <kira.bruneau@gmail.com>
2019-03-07 13:11:59 -0500

    Added more packages

--- environment/cli.nix
+++ environment/cli.nix
@@ -5,18 +5,23 @@
     cloc
     curl
     fd
+    file
     fzf
     git
     htop
     jq
+    nethogs
     nix-index
+    nix-prefetch-scripts
     nmap
     python3
     ripgrep
     stow
     sudo
     tmux
+    unzip
     wget
+    xorg.lndir
     youtube-dl
   ];
 }
