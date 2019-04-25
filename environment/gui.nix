7d61202685fd772a506aa4e597d31d9e7012228d
Kira Bruneau <kira.bruneau@gmail.com>
2019-04-25 00:39:37 -0400

    Added common global environment variables

--- environment/gui.nix
+++ environment/gui.nix
@@ -15,4 +15,17 @@
     keepassxc
     speedcrunch
   ];
+
+  environment.variables = {
+    # Use GTK 3 settings in Qt 5
+    # https://wiki.archlinux.org/index.php/Uniform_look_for_Qt_and_GTK_applications
+    QT_QPA_PLATFORMTHEME = "gtk3";
+
+    # Improve appearance of Java applications
+    # https://wiki.archlinux.org/index.php/Java#Tips_and_tricks
+    _JAVA_OPTIONS = "-Dawt.useSystemAAFontSettings=on -Dswing.aatext=true -Dswing.crossplatformlaf=com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
+
+    # Touchscreen support on Firefox
+    MOZ_USE_XINPUT2 = "1";
+  };
 }
--- environment/hidpi.nix
+++ environment/hidpi.nix
@@ -8,4 +8,14 @@
   i18n = {
     consoleFont = "ter-m32n";
   };
+
+  environment.variables = {
+    # GDK 3 virtual pixel scaling
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
+
+    # Qt 5 virtual pixel scaling
+    QT_AUTO_SCREEN_SCALE_FACTOR = "0";
+    QT_SCREEN_SCALE_FACTORS = "2";
+  };
 }
