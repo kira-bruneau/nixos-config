063f39bfd26848573bc4ee880fa94812c98ffc6d
Kira Bruneau <kira.bruneau@gmail.com>
2019-04-25 23:01:11 -0400

    Use pkill for launch script

--- launch.sh
+++ launch.sh
@@ -1,12 +1,7 @@
-#!/usr/bin/env sh
+#!/bin/sh
 
 # Terminate already running bar instances
-killall -q polybar
+pkill polybar
 
-# Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
-
-# Launch example
+# Launch bar with matching hostname
 polybar $(hostname)
-
-echo "Bars launched..."
