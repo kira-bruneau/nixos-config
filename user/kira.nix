66c0d71fe8b4f61b1a8642ba9439d8653d410f8b
Kira Bruneau <kira.bruneau@gmail.com>
2019-05-05 11:09:48 -0400

    Run compton and redshift as systemd services

--- user/kira.nix
+++ user/kira.nix
@@ -29,7 +29,6 @@
 
     # Packages required by my i3 config.
     alacritty
-    compton
     dunst
     feh
     keepassxc
@@ -39,7 +38,6 @@
       i3Support = true;
       pulseSupport = true;
     })
-    redshift
     rofi
     scrot
     sound-theme-freedesktop
@@ -52,7 +50,29 @@
   ];
 
   # Services required by my i3 config.
-  services.geoclue2.enable = true;
+  services.compton = {
+    enable = true;
+    backend = "glx";
+    shadow = true;
+    shadowExclude = [
+      "class_i = 'i3-frame'" # titlebars
+      "name = 'Polybar tray window'" # polybar tray
+      "_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'" # background windows in tabbed layout
+    ];
+    fade = true;
+    fadeDelta = 10; # 100 steps per second
+    fadeSteps = [ "0.0666" "0.0444" ]; # ~150ms ~225ms
+    extraOptions = ''
+      clear-shadow = true;
+      blur-background = true;
+      no-fading-destroyed-argb = true;
+    '';
+  };
+
+  services.redshift = {
+    enable = true;
+    provider = "geoclue2";
+  };
 
   # TODO: automatically clone dotfiles / emacs config to home
   # See: https://github.com/rycee/home-manager
