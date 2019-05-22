2fa4ce482017cd9992a4a54105798c5815bbf083
Kira Bruneau <kira.bruneau@gmail.com>
2019-05-22 17:32:21 -0400

    Rebind xref find definition/references to lsp equivalents

--- setup/minor-modes/lsp.el
+++ setup/minor-modes/lsp.el
@@ -4,5 +4,8 @@
 (use-package lsp-ui
   :straight t
   :hook ((lsp-mode . lsp-ui-mode))
+  :bind (:map lsp-ui-mode-map
+              ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
+              ([remap xref-find-references] . lsp-ui-peek-find-references))
   :config
   (setq lsp-ui-sideline-enable nil))
