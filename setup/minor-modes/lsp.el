197eea1f630b050d3637bcc0b5a06cce7407e9ea
Kira Bruneau <kira.bruneau@gmail.com>
2019-05-23 23:29:08 -0400

    Use C-r for lsp-rename since I use swiper instead of isearch

--- setup/minor-modes/lsp.el
+++ setup/minor-modes/lsp.el
@@ -6,7 +6,9 @@
 (use-package lsp-ui
   :straight t
   :hook (lsp-mode . lsp-ui-mode)
-  :bind (:map lsp-ui-mode-map
+  :bind (:map lsp-mode-map
+              ("C-r" . lsp-rename)
+         :map lsp-ui-mode-map
               ([remap xref-find-definitions] . lsp-ui-peek-find-definitions)
               ([remap xref-find-references] . lsp-ui-peek-find-references))
   :config
--- setup/minor-modes/swiper.el
+++ setup/minor-modes/swiper.el
@@ -1,6 +1,9 @@
 (use-package swiper
   :straight t
   :after ivy
-  :bind (("C-s" . swiper))
+  :bind (("C-s" . swiper)
+         ("C-r" . nil)
+         ("C-S-s" . isearch-forward)
+         ("C-S-r" . isearch-backward))
   :config
   (add-to-list 'ivy-re-builders-alist '(swiper . ivy--regex-plus)))
