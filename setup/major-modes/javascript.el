26824c110ef11fcf9d6ef5ada5d5147ccfb1ba56
Kira Bruneau <kira.bruneau@gmail.com>
2019-05-27 23:14:58 -0400

    Use typescript-language-server for both js and ts

--- setup/major-modes/javascript.el
+++ setup/major-modes/javascript.el
@@ -2,41 +2,7 @@
   :straight t
   :mode "\\.js\\'"
   :interpreter "node"
-  :bind (:map js2-mode-map
-              ("C-c C-t" . nil)
-              ("C-c c" . js2-mode-toggle-hide-comments))
+  :hook (js2-mode . lsp)
   :init
   (with-eval-after-load 'restclient
     (add-to-list 'restclient-content-type-modes '("application/js" . js2-mode))))
-
-(use-package tern
-  :straight t
-  :ensure-system-package
-  ((npm . npm)
-   (npm . nodejs)
-   (tern . "npm i -g tern"))
-  :after js2-mode
-  :diminish " ◎"
-  :hook (js2-mode . tern-mode)
-  :config
-  (defun tern-setup-project-browser ()
-    "Setup a new browser tern project"
-    (interactive)
-    (let ((source (concat dir/conf "browser.tern-project"))
-          (dest (concat default-directory ".tern-project")))
-      (copy-file source dest)
-      (find-file dest)))
-
-  (defun tern-setup-project-node ()
-    "Setup a new node.js tern project"
-    (interactive)
-    (let ((source (concat dir/conf "node.tern-project"))
-          (dest (concat default-directory ".tern-project")))
-      (copy-file source dest)
-      (find-file dest))))
-
-(use-package company-tern
-  :straight t
-  :after (company tern)
-  :init
-  (add-to-list 'company-backends 'company-tern))
--- setup/major-modes/typescript.el
+++ setup/major-modes/typescript.el
@@ -1,14 +1,3 @@
 (use-package typescript-mode
-  :straight t)
-
-(use-package tide
   :straight t
-  :after typescript-mode
-  :hook
-  ((typescript-mode . (lambda ()
-                        (tide-setup)
-                        (tide-hl-identifier-mode)))
-   (tide-mode . (lambda ()
-                  (flycheck-mode)
-                  (company-mode)
-                  (eldoc-mode)))))
+  :hook (typescript-mode . lsp))
