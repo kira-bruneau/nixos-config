510e1bb4f5d29408a4a6d75d229cd6970a758096
Kira Bruneau <kira.bruneau@gmail.com>
2019-08-21 16:44:12 -0400

    Added language server for nix-mode

--- setup/major-modes/nix.el
+++ setup/major-modes/nix.el
@@ -1,2 +1,12 @@
 (use-package nix-mode
-  :straight t)
+  :straight t
+  :hook (nix-mode . (lambda () (when (executable-find "nix-lsp") (lsp))))
+  :config
+  (require 'lsp-mode)
+
+  (lsp-register-client
+   (make-lsp-client :new-connection (lsp-stdio-connection "nix-lsp")
+                    :major-modes '(nix-mode)
+                    :server-id 'nix-lsp))
+
+  (add-to-list 'lsp-language-id-configuration '(nix-mode . "nix")))
