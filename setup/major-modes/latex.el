384bd2d8f37aaf9c5314db1a2e35f802358876ac
Kira Bruneau <kira.bruneau@gmail.com>
2019-09-03 15:57:57 -0400

    Added texlab language server

--- setup/major-modes/latex.el
+++ setup/major-modes/latex.el
@@ -1,9 +1,20 @@
-;; (use-package tex
-;;   :ensure auctex)
+(require 'lsp-mode)
 
-(use-package tex-mode
+(lsp-register-client
+ (make-lsp-client :new-connection (lsp-stdio-connection "/tmp/nix/texlab/target/release/texlab")
+                  :major-modes '(latex-mode bibtex-mode)
+                  :server-id 'texlab))
+
+(add-to-list 'lsp-language-id-configuration '(latex-mode . "latex"))
+(add-to-list 'lsp-language-id-configuration '(bibtex-mode . "bibtex"))
+
+(use-package latex-mode
   :bind (:map latex-mode-map
-              ("M-p" . latex-preview-pane-mode)))
+              ("M-p" . latex-preview-pane-mode))
+  :hook (latex-mode . lsp))
+
+(use-package bibtex-mode
+  :hook (bibtex-mode . lsp))
 
 (use-package latex-preview-pane
   :straight t
--- setup/major-modes/nix.el
+++ setup/major-modes/nix.el
@@ -1,12 +1,12 @@
+(require 'lsp-mode)
+
+(lsp-register-client
+ (make-lsp-client :new-connection (lsp-stdio-connection "nix-lsp")
+                  :major-modes '(nix-mode)
+                  :server-id 'nix-lsp))
+
+(add-to-list 'lsp-language-id-configuration '(nix-mode . "nix"))
+
 (use-package nix-mode
   :straight t
-  :hook (nix-mode . (lambda () (when (executable-find "nix-lsp") (lsp))))
-  :config
-  (require 'lsp-mode)
-
-  (lsp-register-client
-   (make-lsp-client :new-connection (lsp-stdio-connection "nix-lsp")
-                    :major-modes '(nix-mode)
-                    :server-id 'nix-lsp))
-
-  (add-to-list 'lsp-language-id-configuration '(nix-mode . "nix")))
+  :hook (nix-mode . lsp))
