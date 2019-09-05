bea2dd38a6b432144c54bc3ad3af4c28e5af4448
Kira Bruneau <kira.bruneau@gmail.com>
2019-09-05 18:12:22 -0400

    Set hidpi scaling variables globally
    This was reverted so that GUIs launched from systemd services are
    scaled properly. Scaling in sway will be broken by this change.

--- environment/hidpi.nix
+++ environment/hidpi.nix
@@ -2,18 +2,16 @@
 
 {
   i18n.consoleFont = "${pkgs.terminus_font}/share/consolefonts/ter-132n";
-  services.xserver = {
-    dpi = 192;
-    displayManager.sessionCommands = ''
-      # GDK 3+ virtual pixel scaling
-      export GDK_SCALE=2
-      export GDK_DPI_SCALE=0.5
+  services.xserver.dpi = 192;
+  environment.variables = {
+    # GDK 3+ virtual pixel scaling
+    GDK_SCALE = "2";
+    GDK_DPI_SCALE = "0.5";
 
-      # Qt 5+ virtual pixel scaling
-      export QT_AUTO_SCREEN_SCALE_FACTOR=1
+    # Qt 5+ virtual pixel scaling
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
 
-      # winit virtual pixel scaling
-      export WINIT_HIDPI_FACTOR=2
-    '';
+    # winit virtual pixel scaling
+    WINIT_HIDPI_FACTOR = "2";
   };
 }
