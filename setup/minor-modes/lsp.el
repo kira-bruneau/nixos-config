eb90e9b0efc0e994817e824c9a9d8c309ecb498d
Kira Bruneau <kira.bruneau@gmail.com>
2020-02-01 10:01:56 -0500

    Only show lsp ui doc with keybinding

--- setup/minor-modes/lsp.el
+++ setup/minor-modes/lsp.el
@@ -1,20 +1,23 @@
 (use-package lsp-mode
   :straight t
+  :bind (:map lsp-mode-map
+              ("C-r" . lsp-rename))
   :config
-  (setq lsp-prefer-flymake nil)
+  (setq lsp-enable-file-watchers nil) ;; file watchers cause emacs to hang on large projects
   (setq lsp-enable-snippet nil)
-  (setq lsp-enable-file-watchers nil)) ;; file watchers cause emacs to hang on large projects
+  (setq lsp-prefer-flymake nil))
 
 (use-package lsp-ui
   :straight t
   :hook (lsp-mode . lsp-ui-mode)
-  :bind (:map lsp-mode-map
-              ("C-r" . lsp-rename)
-         :map lsp-ui-mode-map
+  :bind (:map lsp-ui-mode-map
               ("M-." . lsp-ui-peek-find-definitions)
               ("C-M-." . lsp-find-definition)
-              ("M-?" . lsp-ui-peek-find-references)
-              ("C-M-?" . lsp-find-references))
+              ("M-/" . lsp-ui-peek-find-references)
+              ("C-M-/" . lsp-find-references)
+              ("M-?" . lsp-ui-doc-glance))
   :config
-  (setq lsp-ui-sideline-enable nil)
-  (setq lsp-ui-flycheck-enable t))
+  (setq lsp-ui-doc-delay 0)
+  (setq lsp-ui-doc-enable nil)
+  (setq lsp-ui-flycheck-enable t)
+  (setq lsp-ui-sideline-enable nil))
