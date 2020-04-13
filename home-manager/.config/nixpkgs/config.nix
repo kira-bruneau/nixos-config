893a310ca2bb76b9e2f5afcd890e35b33cc039d5
Kira Bruneau <kira.bruneau@gmail.com>
2020-04-13 19:34:37 -0400

    NUR: Updated

--- home-manager/.config/nixpkgs/config.nix
+++ home-manager/.config/nixpkgs/config.nix
@@ -1,8 +1,8 @@
 {
   packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball {
-      url = "https://github.com/nix-community/NUR/archive/7fb601e8ea7e99a46478b55820456259608e91bb.tar.gz";
-      sha256 = "133sby2bv2bqagrgjcih45cx8anjffigdyf7d26rhpp43rpzhmcr";
+      url = "https://github.com/nix-community/NUR/archive/d55b83d1ed97e6bc4eee12dd2982a6c4b9c572de.tar.gz";
+      sha256 = "1dvdymdrw8sn2hsz2isvs37dm4s32ffqa4x64lj2i3d0bd5wfcav";
     }) {
       inherit pkgs;
     };
