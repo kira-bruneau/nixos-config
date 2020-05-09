2be2149dec058ccca2d19dd93fe350927ccff8b0
Kira Bruneau <kira.bruneau@gmail.com>
2020-05-09 15:44:15 -0400

    Isolate home.nix dependencies into a custom profile

--- home.nix
+++ home.nix
@@ -1,75 +1,49 @@
 { config, pkgs, ... }:
 
-let
-  emacs = config.programs.emacs.package;
+{
+  programs.emacs = {
+    enable = true;
+    package = pkgs.callPackage ./wrapper.nix {
+      profile = pkgs.buildEnv {
+        name = "emacs-profile";
+        paths = with pkgs; [
+          (aspellWithDicts (dicts: with dicts; [
+            en
+            en-computers
+            en-science
+          ]))
+          ccls
+          diffutils
+          fd
+          fzf
+          gdb
+          git
+          imagemagick
+          jdk
+          nodejs
+          nodePackages.bash-language-server
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+          pandoc
+          (python3.withPackages(pkgs: with pkgs; [
+            nur.repos.metadark.ptvsd
+            python-language-server
+          ]))
+          ripgrep
+          rnix-lsp
+          rust-analyzer
+          rustc
+          tectonic
+          texlab
+        ];
+      };
 
-  # Create emacseditor script and desktop file for emacsclient
-  # Taken from nixpkgs/nixos/modules/services/editors/emacs.nix
-  editorScript = pkgs.writeScriptBin "emacseditor" ''
-    #!${pkgs.runtimeShell}
-    if [ -z "$1" ]; then
-      exec ${emacs}/bin/emacsclient --create-frame --alternate-editor ${emacs}/bin/emacs
-    else
-      exec ${emacs}/bin/emacsclient --alternate-editor ${emacs}/bin/emacs "$@"
-    fi
-  '';
-
-  desktopApplicationFile = pkgs.writeTextFile {
-    name = "emacsclient.desktop";
-    destination = "/share/applications/emacsclient.desktop";
-    text = ''
-      [Desktop Entry]
-      Name=Emacsclient
-      GenericName=Text Editor
-      Comment=Edit text
-      MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;
-      Exec=emacseditor %F
-      Icon=emacs
-      Type=Application
-      Terminal=false
-      Categories=Development;TextEditor;
-      StartupWMClass=Emacs
-      Keywords=Text;Editor;
-    '';
+      makeWrapperArgs = [
+        # Set default rust source path for rust-analyzer
+        "--set-default RUST_SRC_PATH=${pkgs.rustPackages.rustPlatform.rustcSrc}"
+      ];
+    };
   };
-in {
-  programs.emacs.enable = true;
+
   services.emacs.enable = true;
-
-  home.packages = with pkgs; [
-    aspell
-    aspellDicts.en
-    aspellDicts.en-computers
-    aspellDicts.en-science
-    ccls
-    desktopApplicationFile
-    diffutils
-    editorScript
-    fd
-    fzf
-    gdb
-    git
-    imagemagick
-    jdk
-    nodejs
-    nodePackages.bash-language-server
-    nodePackages.typescript
-    nodePackages.typescript-language-server
-    pandoc
-    python37Packages.python-language-server
-    ripgrep
-    rnix-lsp
-    rust-analyzer
-    rustc
-    tectonic
-    texlab
-  ];
-
-  home.sessionVariables = {
-    # Set rust source path for rust-analyzer
-    RUST_SRC_PATH = pkgs.rustPackages.rustPlatform.rustcSrc;
-
-    # Install ptvsd
-    PYTHONPATH = "${pkgs.nur.repos.metadark.ptvsd}/lib/python3.7/site-packages:$PYTHONPATH";
-  };
 }
---
+++ wrapper.nix
@@ -0,0 +1,69 @@
+{ profile, makeWrapperArgs ? [],
+  writeScriptBin, runtimeShell, writeTextFile,
+  runCommand, lib, emacs, makeWrapper
+}:
+
+let
+  # Create emacseditor script and desktop file for emacsclient
+  # Taken from nixpkgs/nixos/modules/services/editors/emacs.nix
+  editorScript = writeScriptBin "emacseditor" ''
+    #!${runtimeShell}
+    if [ -z "$1" ]; then
+      exec emacsclient --create-frame --alternate-editor ${emacs}/bin/emacs
+    else
+      exec emacsclient --alternate-editor ${emacs}/bin/emacs "$@"
+    fi
+  '';
+
+  desktopApplicationFile = writeTextFile {
+    name = "emacsclient.desktop";
+    destination = "/share/applications/emacsclient.desktop";
+    text = ''
+      [Desktop Entry]
+      Name=Emacsclient
+      GenericName=Text Editor
+      Comment=Edit text
+      MimeType=text/english;text/plain;text/x-makefile;text/x-c++hdr;text/x-c++src;text/x-chdr;text/x-csrc;text/x-java;text/x-moc;text/x-pascal;text/x-tcl;text/x-tex;application/x-shellscript;text/x-c;text/x-c++;
+      Exec=emacseditor %F
+      Icon=emacs
+      Type=Application
+      Terminal=false
+      Categories=Development;TextEditor;
+      StartupWMClass=Emacs
+      Keywords=Text;Editor;
+    '';
+  };
+in runCommand
+  "${emacs.name}-profile-wrapper"
+  {
+    nativeBuildInputs = [ makeWrapper ];
+  }
+  ''
+    mkdir -p "$out"
+    ln -s "${emacs}/"* "$out"
+
+    rm "$out/bin"
+    mkdir "$out/bin"
+    ln -s "${emacs}/bin/"* "$out/bin"
+
+    for bin in "${emacs.name}" emacsclient; do
+      rm "$out/bin/$bin"
+      makeWrapper "${emacs}/bin/$bin" "$out/bin/$bin" \
+        --prefix NIX_PROFILES ' ' "${profile}" \
+        --prefix PATH : "${profile}/bin" \
+        ${lib.concatStringsSep " " makeWrapperArgs}
+    done
+
+    rm "$out/bin/emacs"
+    ln -s "$out/bin/${emacs.name}" "$out/bin/emacs"
+    cp "${editorScript}/bin/emacseditor" "$out/bin"
+
+    rm "$out/share"
+    mkdir "$out/share"
+    ln -s "${emacs}/share/"* "$out/share"
+
+    rm "$out/share/applications"
+    mkdir "$out/share/applications"
+    ln -s "${emacs}/share/applications/"* "$out/share/applications"
+    cp "${desktopApplicationFile}/share/applications/emacsclient.desktop" "$out/share/applications"
+  ''
