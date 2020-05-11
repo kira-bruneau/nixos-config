8150f7278c0fe62a883d6ceaa50f0ebe247b530c
Kira Bruneau <kira.bruneau@gmail.com>
2020-05-10 22:35:06 -0400

    emacs: Updated

--- home-manager/.config/nixpkgs/config.nix
+++ home-manager/.config/nixpkgs/config.nix
@@ -1,8 +1,8 @@
 {
   packageOverrides = pkgs: {
     nur = import (builtins.fetchTarball {
-      url = "https://github.com/nix-community/NUR/archive/d55b83d1ed97e6bc4eee12dd2982a6c4b9c572de.tar.gz";
-      sha256 = "1dvdymdrw8sn2hsz2isvs37dm4s32ffqa4x64lj2i3d0bd5wfcav";
+      url = "https://github.com/nix-community/NUR/archive/9942ee7eed49a8a4e819babda5bb91a433b758f7.tar.gz";
+      sha256 = "01sxrw42ddalrgsflq6vlavqf36a3q7pbqgqd7zxim392mrbiaqj";
     }) {
       inherit pkgs;
     };
