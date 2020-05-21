1be85c4fc99bf04f47d7426683fc99d08aec7e27
Kira Bruneau <kira.bruneau@gmail.com>
2020-05-21 19:00:05 -0400

    dired, image-dired: Add keybinding for image-dired + use large icons

--- setup/major-modes/dired.el
+++ setup/major-modes/dired.el
@@ -1,4 +1,14 @@
 (use-package dired
+  :bind (:map dired-mode-map
+              ("I" . image-dired-default-directory))
   :config
   (setq dired-dwim-target t)
-  (setq dired-listing-switches "-alh"))
+  (setq dired-listing-switches "-alh")
+
+  (defun image-dired-default-directory ()
+    (interactive)
+    (image-dired default-directory)))
+
+(use-package image-dired
+  :config
+  (setq image-dired-thumbnail-storage 'standard-large))
