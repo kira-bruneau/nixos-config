9eb016efe3ed7e34d9c3aa7d6991aedd9c175735
Kira Bruneau <kira.bruneau@gmail.com>
2020-07-17 23:44:29 -0400

    Manage NUR with nix-channel

--- environment/nur.nix
+++ environment/nur.nix
@@ -1,10 +1,7 @@
 { config, pkgs, ... }:
 
 let
-  nur = import (builtins.fetchTarball {
-    url = "https://github.com/nix-community/NUR/archive/d2317cc7fbc5a07fec3491a1d87d64a03b0d8291.tar.gz";
-    sha256 = "1hasha7ggz3zf6jxafj62fbm3l7bcqzpm71jdjsh57p7wzz8fx80";
-  }) {
+  nur = import <nur> {
     inherit pkgs;
   };
 in rec {
