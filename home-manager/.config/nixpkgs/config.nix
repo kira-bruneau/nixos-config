fa3a2cd27746a994a681dc236eef2a29db9a04ea
Kira Bruneau <kira.bruneau@gmail.com>
2020-07-17 23:47:40 -0400

    Manage NUR with nix-channel

--- home-manager/.config/nixpkgs/config.nix
+++ home-manager/.config/nixpkgs/config.nix
@@ -1,9 +1,6 @@
 {
   packageOverrides = pkgs: {
-    nur = import (builtins.fetchTarball {
-      url = "https://github.com/nix-community/NUR/archive/1313ec5bc0d925b16eb58c40999af9044ac40d0d.tar.gz";
-      sha256 = "1wzhzzi0213dnck2zqarajqa5fmxraqmlqvmj97mw1b4h84615w2";
-    }) {
+    nur = import <nur> {
       inherit pkgs;
     };
   };
