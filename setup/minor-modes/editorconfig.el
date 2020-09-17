1d409f152b7809368d395820f393516c88ac7b8e
Kira Bruneau <kira.bruneau@gmail.com>
2020-09-16 21:27:19 -0400

    editorconfig: Ignore indent_size/tab_width when indent_style is tab

--- setup/minor-modes/editorconfig.el
+++ setup/minor-modes/editorconfig.el
@@ -1,4 +1,12 @@
+(defun my-editorconfig-hack-properties (props)
+  ;; Ignore indent_size/tab_width when indent_style is tab
+  (when (string-equal (gethash 'indent_style props) "tab")
+    (remhash 'indent_size props)
+    (remhash 'tab_width props)))
+
 (use-package editorconfig
   :straight t
   :init
-  (editorconfig-mode 1))
+  (editorconfig-mode 1)
+  (add-hook 'editorconfig-hack-properties-functions
+            #'my-editorconfig-hack-properties))
