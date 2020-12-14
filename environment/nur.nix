c152b792a3cbd022009437b4a5c18d6554a0da5e
Kira Bruneau <kira.bruneau@gmail.com>
2020-12-13 22:38:04 -0500

    Revert "Use nur without pkgs for modules"
    This reverts commit 771ee60fa665889ff2472711209c9888877d4729.

--- environment/nur.nix
+++ environment/nur.nix
@@ -1,13 +1,14 @@
 { config, pkgs, ... }:
 
 let
-  nur-no-pkgs = import <nur> { pkgs = null; };
-  nur = import <nur> { inherit pkgs; };
+  nur = import <nur> {
+    inherit pkgs;
+  };
 in rec {
   imports = [
     ../cachix.nix
-    nur-no-pkgs.repos.metadark.modules.bluetooth-autoconnect
-    nur-no-pkgs.repos.metadark.modules.lightdm-webkit2-greeter
+    nur.repos.metadark.modules.bluetooth-autoconnect
+    nur.repos.metadark.modules.lightdm-webkit2-greeter
   ];
 
   nixpkgs.config.packageOverrides = pkgs: {
