0010b186ca1aa2eb18caea25f5b57563aeabcd1a
Kira Bruneau <kira.bruneau@gmail.com>
2021-01-30 14:52:47 -0500

    Harden SSH authentication

--- machine/atlantis.nix
+++ machine/atlantis.nix
@@ -30,7 +30,11 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    passwordAuthentication = false;
+    permitRootLogin = "no";
+  };
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
