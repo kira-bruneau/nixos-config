d095429984750460a1bf0278c80877d9767a99f7
Kira Bruneau <kira.bruneau@pm.me>
2021-05-24 13:27:42 -0400

    wrapper.nix: cleanup

--- wrapper.nix
+++ wrapper.nix
@@ -1,6 +1,12 @@
-{ profile, makeWrapperArgs ? [],
-  writeScriptBin, runtimeShell, writeTextFile,
-  runCommand, lib, emacs, makeWrapper
+{ lib
+, writeScriptBin
+, runtimeShell
+, writeTextFile
+, runCommand
+, makeWrapper
+, emacs
+, profile
+, makeWrapperArgs ? [],
 }:
 
 let
@@ -39,32 +45,27 @@ in runCommand
     nativeBuildInputs = [ makeWrapper ];
   }
   ''
-    mkdir -p "$out"
-    ln -s "${emacs}/"* "$out"
+    mkdir "$out"
+    ln -s ${emacs}/* "$out"
 
-    rm "$out/bin"
-    mkdir "$out/bin"
-    ln -s "${emacs}/bin/"* "$out/bin"
+    rm "$out"/bin; mkdir "$out"/bin
+    ln -s ${emacs}/bin/* "$out"/bin
 
-    for bin in "${emacs.name}" emacsclient; do
-      rm "$out/bin/$bin"
-      makeWrapper "${emacs}/bin/$bin" "$out/bin/$bin" \
-        --prefix NIX_PROFILES ' ' "${profile}" \
-        --prefix PATH : "${profile}/bin" \
-        --run 'if [ -e $HOME/.profile ]; then . $HOME/.profile; fi' \
+    for bin in $(basename "$out"/bin/emacs-*) emacsclient; do
+      rm "$out"/bin/"$bin"
+      makeWrapper ${emacs}/bin/"$bin" "$out"/bin/"$bin" \
+        --prefix NIX_PROFILES ' ' ${profile} \
+        --prefix PATH : ${profile}/bin \
         ${lib.concatStringsSep " " makeWrapperArgs}
     done
 
-    rm "$out/bin/emacs"
-    ln -s "$out/bin/${emacs.name}" "$out/bin/emacs"
-    cp "${editorScript}/bin/emacseditor" "$out/bin"
+    ln -sf "$out"/bin/emacs-* "$out"/bin/emacs
+    cp ${editorScript}/bin/emacseditor "$out"/bin
 
-    rm "$out/share"
-    mkdir "$out/share"
-    ln -s "${emacs}/share/"* "$out/share"
+    rm "$out"/share; mkdir "$out"/share
+    ln -s ${emacs}/share/* "$out"/share
 
-    rm "$out/share/applications"
-    mkdir "$out/share/applications"
-    ln -s "${emacs}/share/applications/"* "$out/share/applications"
-    cp -f "${desktopApplicationFile}/share/applications/emacsclient.desktop" "$out/share/applications"
+    rm "$out"/share/applications; mkdir "$out"/share/applications
+    ln -s ${emacs}/share/applications/* "$out"/share/applications
+    cp -f ${desktopApplicationFile}/share/applications/emacsclient.desktop "$out"/share/applications
   ''
