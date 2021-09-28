95a5b837f3c6914735e7d7e51428edd694de9ed0
Kira Bruneau <kira.bruneau@pm.me>
2021-09-27 21:49:47 -0400

    replace prettier-mode with apheleia-mode

---
+++ setup/minor-modes/apheleia.el
@@ -0,0 +1,4 @@
+(use-package apheleia
+  :straight t
+  :init
+  (apheleia-global-mode t))
--- setup/minor-modes/prettier.el
+++ setup/minor-modes/prettier.el
@@ -1,6 +1,6 @@
-(use-package prettier-js
-  :straight t
-  :hook ((js2-mode . prettier-js-mode)
-         (json-mode . prettier-js-mode)
-         (typescript-mode . prettier-js-mode)
-         (web-mode . prettier-js-mode)))
+;; (use-package prettier-js
+;;   :straight t
+;;   :hook ((js2-mode . prettier-js-mode)
+;;          (json-mode . prettier-js-mode)
+;;          (typescript-mode . prettier-js-mode)
+;;          (web-mode . prettier-js-mode)))
