3bab9f307d6970a3ec95c9517b20a249b00345ea
Kira Bruneau <kira.bruneau@pm.me>
2021-10-26 14:00:24 -0400

    package/gpg: setup symlinks to auth files

--- package/gpg/default.nix
+++ package/gpg/default.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 {
   programs.gpg.enable = true;
@@ -8,5 +8,20 @@
     defaultCacheTtl = 240;
     defaultCacheTtlSsh = 240;
     pinentryFlavor = "gnome3";
+  };
+
+  # Manage gpg keys outside of home-manager (synced with Syncthing)
+  home.file = {
+    ".authinfo.gpg".source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/Auth/.authinfo.gpg";
+
+    ".gnupg/openpgp-revocs.d".source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/Auth/gnupg/openpgp-revocs.d";
+
+    ".gnupg/private-keys-v1.d".source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/Auth/gnupg/private-keys-v1.d";
+
+    ".gnupg/pubring.kbx".source = config.lib.file.mkOutOfStoreSymlink
+      "${config.home.homeDirectory}/Auth/gnupg/pubring.kbx";
   };
 }
