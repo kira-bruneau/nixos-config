f946238ac73570f715b500f56625ffb17f1e7043
Kira Bruneau <kira.bruneau@pm.me>
2021-10-26 09:48:55 -0400

    environment/wireless: remove MPRIS proxy (doesn't work)

--- environment/wireless.nix
+++ environment/wireless.nix
@@ -7,14 +7,4 @@
   # Enable bluetooth support
   hardware.bluetooth.enable = true;
   services.blueman.enable = true;
-
-  # Enable bluetooth MPRIS proxy
-  systemd.user.services.mpris-proxy = {
-    description = "MPRIS proxy";
-    wantedBy = [ "default.target" ];
-    after = [ "network.target" "sound.target" ];
-    serviceConfig = {
-      ExecStart = "${pkgs.bluez}/bin/mpris-proxy";
-    };
-  };
 }
