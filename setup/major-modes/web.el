100ac6a7251231d847fe991f8e040434e6f4539a
Kira Bruneau <kira.bruneau@pm.me>
2022-02-21 18:50:25 -0500

    remove web-beautify
    replaced with apheleia-mode

--- setup/major-modes/web.el
+++ setup/major-modes/web.el
@@ -15,21 +15,3 @@
 
   :config
   (setq web-mode-enable-current-element-highlight t))
-
-(use-package web-beautify
-  :straight t
-  :init
-  (with-eval-after-load 'js2-mode
-    (define-key js2-mode-map (kbd "C-c b") 'web-beautify-js))
-
-  (with-eval-after-load 'json-mode
-    (define-key json-mode-map (kbd "C-c b") 'web-beautify-js))
-
-  (with-eval-after-load 'sgml-mode
-    (define-key html-mode-map (kbd "C-c b") 'web-beautify-html))
-
-  (with-eval-after-load 'web-mode
-    (define-key web-mode-map (kbd "C-c b") 'web-beautify-html))
-
-  (with-eval-after-load 'css-mode
-    (define-key css-mode-map (kbd "C-c b") 'web-beautify-css)))
