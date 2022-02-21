c554779a46ff48bd15b4ef45fb988f18d73eef3a
Kira Bruneau <kira.bruneau@pm.me>
2022-02-21 16:59:23 -0500

    use javascript-eslint flycheck checker instead of lsp-eslint

--- setup/major-modes/web.el
+++ setup/major-modes/web.el
@@ -7,6 +7,9 @@
   :bind (:map web-mode-map
               ("C-c C-o" . browse-url-of-file))
   :init
+  (with-eval-after-load 'flycheck
+    (flycheck-add-mode 'javascript-eslint 'web-mode))
+
   (with-eval-after-load 'restclient
     (add-to-list 'restclient-content-type-modes '("text/html" . web-mode)))
 
--- setup/minor-modes/apheleia.el
+++ setup/minor-modes/apheleia.el
@@ -1,5 +1,4 @@
 (use-package apheleia
   :straight t
   :config
-  (add-to-list 'apheleia-formatters '(prettier . ("prettierc" filepath)))
-  (add-to-list 'apheleia-formatters '(eslint . ("eslint_c" "--stdin" "--fix-to-stdout" "--stdin-filename" filepath))))
+  (add-to-list 'apheleia-formatters '(prettier . ("prettierc" filepath))))
