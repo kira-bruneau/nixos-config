baa244300aca74026582c9f59b9003c3ae761013
Kira Bruneau <kira.bruneau@pm.me>
2022-05-18 08:03:09 -0400

    package/firefox: set as default browser

--- package/firefox/default.nix
+++ package/firefox/default.nix
@@ -125,6 +125,15 @@
         type = "Application";
       };
     };
+
+    mimeApps = {
+      enable = true;
+      defaultApplications = {
+        "text/html" = "firefox.desktop";
+        "x-scheme-handler/http" = "firefox.desktop";
+        "x-scheme-handler/https" = "firefox.desktop";
+      };
+    };
   };
 
   home.sessionVariables = {
