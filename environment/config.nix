a84fa3698e31aff58c93cc3bc12ac19756a4e6d9
Kira Bruneau <kira.bruneau@pm.me>
2022-05-19 20:51:42 -0400

    environment/config: fix VERBOSE_ARG in createHomeConfigGitRepo

--- environment/config.nix
+++ environment/config.nix
@@ -14,8 +14,8 @@ with lib;
       lib.hm.dag.entryAfter [ "writeBoundary" ] ''
         if [ ! -e ${configDirectory} ]; then
           $DRY_RUN_CMD mkdir -p $VERBOSE_ARG $(dirname ${configDirectory})
-          $DRY_RUN_CMD cp -R --no-preserve=ownership $VERBOSE ${self} ${configDirectory}
-          $DRY_RUN_CMD chmod -R +w $VERBOSE ${configDirectory}
+          $DRY_RUN_CMD cp -R --no-preserve=ownership $VERBOSE_ARG ${self} ${configDirectory}
+          $DRY_RUN_CMD chmod -R +w $VERBOSE_ARG ${configDirectory}
           $DRY_RUN_CMD cd ${configDirectory}
           $DRY_RUN_CMD ${pkgs.git}/bin/git init -b main
           $DRY_RUN_CMD ${pkgs.git}/bin/git config include.path ../.gitconfig
