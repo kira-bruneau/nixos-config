7745ad40ba9642ea202412227a2fcc3d66095efb
Kira Bruneau <kira.bruneau@pm.me>
2022-05-26 20:14:15 -0400

    environment/desktop: switch from sddm to greetd + gtkgreet

--- environment/desktop.nix
+++ environment/desktop.nix
@@ -14,34 +14,16 @@
         . $HOME/.profile
       fi
     '';
-
-    systemPackages = with pkgs; [
-      (themes.sddm.clairvoyance.override {
-        autoFocusPassword = true;
-      })
-    ];
   };
 
-  # Enable LightDM display manager
-  services.xserver = {
-    enable = true;
-    displayManager = {
-      defaultSession = "sway";
-      sddm = {
-        enable = true;
-        theme = "clairvoyance";
-      };
-    };
-  };
-
-  # Enable Sway window manager
+  # Enable Sway Wayland compositor
   programs.sway = {
     enable = true;
-    extraPackages = with pkgs; [ swaylock swayidle xwayland ];
+    extraPackages = with pkgs; [ swaylock swayidle ];
     wrapperFeatures.gtk = true;
   };
 
-  # Enable i3-gaps window manager
+  # Enable i3-gaps X11 window manager
   services.xserver = {
     useGlamor = true;
     windowManager.i3 = {
@@ -50,6 +32,114 @@
       extraPackages = [];
     };
   };
+
+  # Enable gtkgreet greeter (using Sway as the Wayland compositor)
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session.command =
+        let
+          background = pkgs.fetchurl {
+            url = "https://cdna.artstation.com/p/assets/images/images/041/883/956/large/alena-aenami-wings-hd.jpg";
+            sha256 = "sha256-Taa/mlQ9wN5duE49jbXnLQXu5N9GiQQoew4F0wMFCTA=";
+          };
+
+          # TODO: Update gtkgreet to use transparent background when no background is specified
+          transparent = pkgs.fetchurl {
+            url = "https://upload.wikimedia.org/wikipedia/commons/c/ca/1x1.png";
+            sha256 = "sha256-PrEHktHwx+B+ckgnNUDxlS2aWimW9LXfcKsCbNnwVRc=";
+          };
+
+          style = pkgs.writeText "gtkgreet.css" ''
+            * {
+                color: #eee;
+                text-shadow: 1px 1px 2px #233;
+            }
+
+            #clock {
+                margin-bottom: 4px;
+            }
+
+            entry {
+                color: #444;
+                text-shadow: none;
+                box-shadow: 1px 1px 2px #233;
+                border-width: 2px;
+                padding: 4px 8px;
+                background-color: #eee;
+            }
+
+            button {
+                box-shadow: 1px 1px 2px #233;
+                border-width: 2px;
+                padding: 8px;
+                background-color: #eee;
+            }
+
+            button * {
+                color: #444;
+                text-shadow: none;
+            }
+
+            body {
+                padding-bottom: 4px;
+            }
+
+            combobox button {
+                box-shadow: none;
+                padding: 4px;
+            }
+
+            button.suggested-action * {
+                color: #eee;
+            }
+          '';
+
+          gtkgreet-sway-config = pkgs.writeText "gtkgreet-sway-config" ''
+            output * bg ${background} fill
+            output "Goldstar Company Ltd LG HDR 4K 0x0000B721" scale 2 pos 1712,1003
+            output "Unknown 0x095F 0x00000000" scale 1.5 pos 208,1080
+            output "Unknown HP Z27k G3 CN41223C6P" scale 2 pos 0 0
+
+            input * xkb_layout "us,us"
+            input * xkb_variant "colemak,"
+            input * xkb_options "grp:win_space_toggle"
+
+            bindsym XF86MonBrightnessUp exec --no-startup-id light -A 10
+            bindsym XF86MonBrightnessDown exec --no-startup-id light -U 10
+            bindsym Shift+XF86MonBrightnessUp exec --no-startup-id light -S 100
+            bindsym Shift+XF86MonBrightnessDown exec --no-startup-id light -r -S 1
+            # using volume scroller (really nice with the Corsair Vengeance K95)
+            bindsym Mod1+XF86AudioRaiseVolume exec --no-startup-id light -A 10
+            bindsym Mod1+XF86AudioLowerVolume exec --no-startup-id light -U 10
+            bindsym Mod1+Shift+XF86AudioRaiseVolume exec --no-startup-id light -S 100
+            bindsym Mod1+Shift+XF86AudioLowerVolume exec --no-startup-id light -r -S 1
+
+            set $mode_power l̲ogout | s̲hutdown | r̲eboot
+            mode "$mode_power" {
+              bindsym l exec swaymsg exit
+              bindsym s exec systemctl poweroff
+              bindsym r exec systemctl reboot
+              bindsym --release XF86PowerOff exec systemctl poweroff
+              bindsym Control+Mod1+Delete exec systemctl reboot
+              bindsym Return mode "default"
+              bindsym Escape mode "default"
+            }
+
+            bindsym --release XF86PowerOff mode "$mode_power"
+            bindsym Control+Mod1+Delete mode "$mode_power"
+
+            exec dbus-update-activation-environment --systemd DISPLAY WAYLAND_DISPLAY SWAYSOCK
+            exec "${pkgs.greetd.gtkgreet}/bin/gtkgreet -l -b ${transparent} -s ${style}; ${pkgs.sway}/bin/swaymsg exit"
+          '';
+        in "sway --config ${gtkgreet-sway-config}";
+    };
+  };
+
+  # TODO: Add X + i3
+  environment.etc."greetd/environments".text = ''
+    sway
+  '';
 
   # Redshift
   # TODO: Start at login
