dea3e1ddb17feb0c5a937ef4ba5f50daa7b284f8
Kira Bruneau <kira.bruneau@pm.me>
2022-05-27 18:31:44 -0400

    host/framework: move gnome-keyring setup to nixos config

--- host/framework.nix
+++ host/framework.nix
@@ -13,9 +13,6 @@
 
   services.syncthing.enable = true;
 
-  # Required by arctype to manage passwords
-  services.gnome-keyring.enable = true;
-
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [
     "arctype"
     "unrar"
