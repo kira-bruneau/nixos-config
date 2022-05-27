119721862cdbc4d0a24402739634857522e965c9
Kira Bruneau <kira.bruneau@pm.me>
2022-05-27 18:17:30 -0400

    environment/laptop: replace tlp with auto-cpufreq

--- environment/laptop.nix
+++ environment/laptop.nix
@@ -15,8 +15,20 @@
     };
   };
 
-  # Power management for saving laptop battery
-  services.tlp.enable = true;
+  # Automatically control frequency of CPU to save power
+  environment.systemPackages = [ pkgs.auto-cpufreq ];
+  systemd.services.auto-cpufreq = {
+    description = "auto-cpufreq - Automatic CPU speed & power optimizer for Linux";
+    after = [ "network.target" "network-online.target" ];
+    path = with pkgs; [ ncurses bash ];
+    serviceConfig = {
+      Type = "simple";
+      User = "root";
+      ExecStart = "${pkgs.auto-cpufreq}/bin/auto-cpufreq --daemon";
+      Restart = "on-failure";
+    };
+    wantedBy = [ "multi-user.target" ];
+  };
 
   # Automatically suspend on low power
   services.upower.enable = true;
