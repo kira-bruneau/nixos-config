33a066fcf4c23f3b7918fe2262b8d9aeb32c6834
Kira Bruneau <kira.bruneau@pm.me>
2022-05-29 16:11:21 -0400

    environment/laptop: use nixos module for auto-cpufreq

--- environment/laptop.nix
+++ environment/laptop.nix
@@ -24,19 +24,7 @@
   };
 
   # Automatically control frequency of CPU to save power
-  environment.systemPackages = [ pkgs.auto-cpufreq ];
-  systemd.services.auto-cpufreq = {
-    description = "auto-cpufreq - Automatic CPU speed & power optimizer for Linux";
-    after = [ "network.target" "network-online.target" ];
-    path = with pkgs; [ ncurses bash ];
-    serviceConfig = {
-      Type = "simple";
-      User = "root";
-      ExecStart = "${pkgs.auto-cpufreq}/bin/auto-cpufreq --daemon";
-      Restart = "on-failure";
-    };
-    wantedBy = [ "multi-user.target" ];
-  };
+  services.auto-cpufreq.enable = true;
 
   # Automatically suspend on low power
   services.upower.enable = true;
