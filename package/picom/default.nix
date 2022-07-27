474f89f90880dcfd4e87a7e670b897bc6a163d05
Kira Bruneau <kira.bruneau@pm.me>
2022-07-27 00:58:49 -0400

    flake.lock: update

--- package/picom/default.nix
+++ package/picom/default.nix
@@ -11,16 +11,16 @@
       "_NET_WM_STATE@:32a *= '_NET_WM_STATE_HIDDEN'" # background windows in tabbed layout
       "name = 'cpt_frame_window'" # fix shadow overlaying zoom call
     ];
-    opacityRule = [
+    opacityRules = [
       "90:name = 'Picture-in-Picture'"
     ];
     fade = true;
     fadeDelta = 10; # 100 steps per second
-    fadeSteps = [ "0.0666" "0.0444" ]; # ~150ms ~225ms
-    extraOptions = ''
+    fadeSteps = [ 0.0666 0.0444 ]; # ~150ms ~225ms
+    settings = {
       clear-shadow = true;
       blur-background = true;
       no-fading-destroyed-argb = true;
-    '';
+    };
   };
 }
