fd465a0b7e076f49547aa9154563917315d674c6
Kira Bruneau <kira.bruneau@pm.me>
2023-02-08 16:25:46 -0500

    package/rofi: add rofi-emoji

--- package/rofi/default.nix
+++ package/rofi/default.nix
@@ -6,6 +6,12 @@
     package = pkgs.rofi-wayland;
   };
 
+  home.packages = with pkgs; [
+    (rofimoji.override {
+      rofi = rofi-wayland;
+    })
+  ];
+
   # TODO: Generate configuration from Nix
   xdg.configFile.rofi.source = ./config;
 }
