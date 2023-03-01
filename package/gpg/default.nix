10be46e560dcf5c93080a07108912038a069974a
Kira Bruneau <kira.bruneau@pm.me>
2023-03-01 10:31:51 -0500

    package/gpg: refactor gpg key management to be more portable

--- package/gpg/default.nix
+++ package/gpg/default.nix
@@ -1,9 +1,16 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   programs.gpg = {
     enable = true;
     homedir = "${config.xdg.dataHome}/gnupg";
+    publicKeys = [
+      {
+        # TODO: Refactor home-manager module so it doesn't put the source in the global nix store
+        source = config.lib.file.mkOutOfStoreSymlink
+          "${config.home.homeDirectory}/Auth/kira.asc";
+      }
+    ];
   };
 
   services.gpg-agent = {
@@ -12,20 +19,5 @@
     defaultCacheTtl = 240;
     defaultCacheTtlSsh = 240;
     pinentryFlavor = "gnome3";
-  };
-
-  # Manage authinfo & gpg keys outside of home-manager (synced with Syncthing)
-  xdg.dataFile = {
-    "authinfo/authinfo.gpg".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/Auth/.authinfo.gpg";
-
-    "gnupg/openpgp-revocs.d".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/Auth/gnupg/openpgp-revocs.d";
-
-    "gnupg/private-keys-v1.d".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/Auth/gnupg/private-keys-v1.d";
-
-    "gnupg/pubring.kbx".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.homeDirectory}/Auth/gnupg/pubring.kbx";
   };
 }
