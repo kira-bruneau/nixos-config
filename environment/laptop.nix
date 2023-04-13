acc60a891b21dea0f4b56ca97628f9a4da9f49f1
Kira Bruneau <kira.bruneau@pm.me>
2023-04-13 08:31:03 -0400

    environment/laptop: fix optional sway configs
    error: The option `environment.etc."sway/config.d/backlight-controls.conf".source' is used but not defined.
    error: The option `environment.etc."sway/config.d/sway/config.d/touchpad.conf".source' is used but not defined.

--- environment/laptop.nix
+++ environment/laptop.nix
@@ -1,27 +1,10 @@
-{ lib, config, pkgs, ... }:
+{ config, ... }:
 
 {
   imports = [
     ./desktop.nix
     ./portable.nix
   ];
-
-  # Enable touchpad support
-  environment.etc."sway/config.d/touchpad.conf".text = lib.mkIf config.programs.sway.enable ''
-    input type:touchpad {
-      natural_scroll enabled
-      tap enabled
-      scroll_factor 0.25
-    }
-  '';
-
-  services.xserver.libinput = {
-    enable = true;
-    touchpad = {
-      naturalScrolling = true;
-      tapping = true;
-    };
-  };
 
   # Automatically control frequency of CPU to save power
   services.auto-cpufreq.enable = true;
@@ -32,17 +15,40 @@
   # Automatically suspend on low power
   services.upower.enable = true;
 
+  # Enable touchpad support
+  services.xserver.libinput = {
+    enable = true;
+    touchpad = {
+      naturalScrolling = true;
+      tapping = true;
+    };
+  };
+
+  environment.etc."sway/config.d/touchpad.conf" = {
+    enable = config.programs.sway.enable;
+    text = ''
+      input type:touchpad {
+        natural_scroll enabled
+        tap enabled
+        scroll_factor 0.25
+      }
+    '';
+  };
+
   # Enable light for controlling backlight
   programs.light.enable = true;
-  environment.etc."sway/config.d/backlight-controls.conf".text = lib.mkIf config.programs.sway.enable ''
-    bindsym XF86MonBrightnessUp exec --no-startup-id light -A 10
-    bindsym XF86MonBrightnessDown exec --no-startup-id light -U 10
-    bindsym Shift+XF86MonBrightnessUp exec --no-startup-id light -S 100
-    bindsym Shift+XF86MonBrightnessDown exec --no-startup-id light -r -S 1
-    # using volume scroller (really nice with the Corsair Vengeance K95)
-    bindsym Mod1+XF86AudioRaiseVolume exec --no-startup-id light -A 10
-    bindsym Mod1+XF86AudioLowerVolume exec --no-startup-id light -U 10
-    bindsym Mod1+Shift+XF86AudioRaiseVolume exec --no-startup-id light -S 100
-    bindsym Mod1+Shift+XF86AudioLowerVolume exec --no-startup-id light -r -S 1
-  '';
+  environment.etc."sway/config.d/backlight-controls.conf" = {
+    enable = config.programs.sway.enable;
+    text = ''
+      bindsym XF86MonBrightnessUp exec --no-startup-id light -A 10
+      bindsym XF86MonBrightnessDown exec --no-startup-id light -U 10
+      bindsym Shift+XF86MonBrightnessUp exec --no-startup-id light -S 100
+      bindsym Shift+XF86MonBrightnessDown exec --no-startup-id light -r -S 1
+      # using volume scroller (really nice with the Corsair Vengeance K95)
+      bindsym Mod1+XF86AudioRaiseVolume exec --no-startup-id light -A 10
+      bindsym Mod1+XF86AudioLowerVolume exec --no-startup-id light -U 10
+      bindsym Mod1+Shift+XF86AudioRaiseVolume exec --no-startup-id light -S 100
+      bindsym Mod1+Shift+XF86AudioLowerVolume exec --no-startup-id light -r -S 1
+    '';
+  };
 }
