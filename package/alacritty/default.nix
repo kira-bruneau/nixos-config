7519e8290ec4f204cc6945053093a9a04136c2ae
Kira Bruneau <kira.bruneau@pm.me>
2023-05-28 20:34:58 -0400

    package/alacritty: port config to nix

--- package/alacritty/default.nix
+++ package/alacritty/default.nix
@@ -1,8 +1,48 @@
 { ... }:
 
 {
-  programs.alacritty.enable = true;
-
-  # TODO: Generate configuration from Nix
-  xdg.configFile.alacritty.source = ./config;
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      window = {
+        padding = {
+          x = 8;
+          y = 8;
+        };
+        opacity = 0.95;
+      };
+      font.size = 9.0;
+      colors = {
+        primary = {
+          background = "0x212121";
+          foreground = "0xc0c5ce";
+          bright_foreground = "0xf3f4f5";
+        };
+        cursor = {
+          text = "0x212121";
+          cursor = "0xc0c5ce";
+        };
+        normal = {
+          black = "0x212121";
+          red = "0xe57373";
+          green = "0xa6bc69";
+          yellow = "0xfac863";
+          blue = "0x6699cc";
+          magenta = "0xc594c5";
+          cyan = "0x5fb3b3";
+          white = "0xc0c5ce";
+        };
+        bright = {
+          black = "0x5c5c5c";
+          red = "0xe57373";
+          green = "0xa6bc69";
+          yellow = "0xfac863";
+          blue = "0x6699cc";
+          magenta = "0xc594c5";
+          cyan = "0x5fb3b3";
+          white = "0xf3f4f5";
+        };
+      };
+    };
+  };
 }
