1ba13e422459010a2fd9c6659fd9ca7c9c37afe0
Kira Bruneau <kira.bruneau@pm.me>
2023-05-28 20:19:42 -0400

    package/git: port config to nix

--- package/git/default.nix
+++ package/git/default.nix
@@ -1,12 +1,74 @@
 { pkgs, ... }:
 
 {
-  programs.git.enable = true;
+  programs.git = {
+    enable = true;
+    userName = "Kira Bruneau";
+    userEmail = "kira.bruneau@pm.me";
+    extraConfig = {
+      init.defaultBranch = "main";
+      core.whitespace = "cr-at-eol";
+
+      pull.rebase = "merges";
+      rebase.autostash = true;
+      diff.algorithm = "histogram";
+
+      gitlab.user = "kira-bruneau";
+      github.user = "kira-bruneau";
+
+      "url \"git@gitlab.com:\"" = {
+        pushInsteadOf = [
+          "git://gitlab.com/"
+          "https://gitlab.com/"
+        ];
+      };
+
+      "url \"git@github.com:\"" = {
+        pushInsteadOf = [
+          "git://github.com/"
+          "https://github.com/"
+        ];
+      };
+    };
+
+    ignores = [
+      # Emacs lock files
+      ".[#]*"
+
+      # Emacs autosave files
+      "[#]*[#]"
+
+      # Emacs backup files
+      "*~"
+
+      # Project Management
+      ".ccls-cache"
+      ".direnv"
+      ".envrc"
+      ".idea"
+      ".mypy_cache"
+      "compile_commands.json"
+
+      ## Tern
+      ".tern-port"
+      ".tern-project"
+
+      ## GNU Global
+      "GPATH"
+      "GRTAGS"
+      "GSYMS"
+      "GTAGS"
+      "TAGS"
+
+      ## Eclipse JDT Language Server
+      ".classpath"
+      ".project"
+      ".settings"
+      ".factorypath"
+    ];
+  };
 
   home.packages = with pkgs; [
     gitAndTools.git-bug
   ];
-
-  # TODO: Generate configuration from Nix
-  xdg.configFile.git.source = ./config;
 }
