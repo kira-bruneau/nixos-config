dc337f8858068e7cb8d548ce87fb6cb4291480f2
Kira Bruneau <kira.bruneau@pm.me>
2023-10-06 09:52:11 -0400

    environment/portable: disable unused sources in geoclue

--- environment/portable.nix
+++ environment/portable.nix
@@ -8,5 +8,20 @@
 
   location.provider = "geoclue2";
 
+  # Disable unused geoclue sources
+  services.geoclue2 = {
+    # WiFi (requires wpa_supplicant)
+    # Failed to query location: No WiFi networks found
+    enableWifi = false;
+
+    # GPS (requires Avahi)
+    enableNmea = false;
+
+    # Cellular (requires ModemManager)
+    enable3G = false;
+    enableCDMA = false;
+    enableModemGPS = false;
+  };
+
   services.localtimed.enable = true;
 }
