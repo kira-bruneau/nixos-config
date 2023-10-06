5b61bca727167d480e0d54b867cd176e348bbdcf
Kira Bruneau <kira.bruneau@pm.me>
2023-10-06 11:36:08 -0400

    environment/bluetooth: allow globally readable bluetooth config

--- environment/bluetooth.nix
+++ environment/bluetooth.nix
@@ -2,4 +2,7 @@
 
 {
   hardware.bluetooth.enable = true;
+
+  # ConfigurationDirectory 'bluetooth' already exists but the mode is different. (File system: 755 ConfigurationDirectoryMode: 555)
+  systemd.services.bluetooth.serviceConfig.ConfigurationDirectoryMode = "0755";
 }
