48744d5d97612d50a111d424e7efde5520200a54
Kira Bruneau <kira.bruneau@pm.me>
2023-10-07 14:33:29 -0400

    package/gtk: port config to nix

--- package/gtk/default.nix
+++ package/gtk/default.nix
@@ -1,32 +1,23 @@
-{ config, pkgs, ... }:
+{ pkgs, ... }:
 
 {
-  home = {
-    packages = with pkgs; [
-      arc-theme
-      arc-icon-theme
-      gnome.adwaita-icon-theme
-    ];
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.arc-theme;
+      name = "Arc";
+    };
+    iconTheme = {
+      package = pkgs.arc-icon-theme;
+      name = "Arc";
+    };
+  };
 
+  home = {
     sessionVariables = {
       # Use GTK 3 settings in Qt 5
       # https://wiki.archlinux.org/index.php/Uniform_look_for_Qt_and_GTK_applications
       QT_QPA_PLATFORMTHEME = "gtk3";
     };
-
-    # Manage GTK2 config outside of home-manager while keeping track of the files in this git repo
-    file = {
-      ".gtkrc-2.0".source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.configDirectory}/package/gtk/gtkrc-2.0";
-
-      ".icons/default/index.theme".source = config.lib.file.mkOutOfStoreSymlink
-        "${config.home.configDirectory}/package/gtk/icon.theme";
-    };
-  };
-
-  # Manage GTK3 config outside of home-manager while keeping track of the files in this git repo
-  xdg.configFile = {
-    "gtk-3.0".source = config.lib.file.mkOutOfStoreSymlink
-      "${config.home.configDirectory}/package/gtk/gtk-3.0";
   };
 }
--- package/sway/default.nix
+++ package/sway/default.nix
@@ -1,22 +1,6 @@
 { pkgs, ... }:
 
 let
-  # Source: https://github.com/swaywm/sway/wiki/GTK-3-settings-on-Wayland#setting-values-in-gsettings
-  import-gsettings = pkgs.writeShellScriptBin "import-gsettings" ''
-    config="''${XDG_CONFIG_HOME:-$HOME/.config}/gtk-3.0/settings.ini"
-    if [ ! -f "$config" ]; then exit 1; fi
-
-    gnome_schema="org.gnome.desktop.interface"
-    gtk_theme="$(grep 'gtk-theme-name' "$config" | sed 's/.*\s*=\s*//')"
-    icon_theme="$(grep 'gtk-icon-theme-name' "$config" | sed 's/.*\s*=\s*//')"
-    cursor_theme="$(grep 'gtk-cursor-theme-name' "$config" | sed 's/.*\s*=\s*//')"
-    font_name="$(grep 'gtk-font-name' "$config" | sed 's/.*\s*=\s*//')"
-    gsettings set "$gnome_schema" gtk-theme "$gtk_theme"
-    gsettings set "$gnome_schema" icon-theme "$icon_theme"
-    gsettings set "$gnome_schema" cursor-theme "$cursor_theme"
-    gsettings set "$gnome_schema" font-name "$font_name"
-  '';
-
   # Randomly choose a wallpaper in ~/Pictures/Wallpapers
   random-wallpaper = pkgs.writeScriptBin "random-wallpaper" ''
     #!${pkgs.python3}/bin/python
@@ -81,7 +65,6 @@ in
     alsa-utils
     font-awesome_5
     grim
-    import-gsettings
     pavucontrol
     random-wallpaper
     scale-off
