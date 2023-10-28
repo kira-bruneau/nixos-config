deebe9bb5dde8accf8dba4ebd4225b9d66998167
Kira Bruneau <kira.bruneau@pm.me>
2023-10-28 17:23:20 -0400

    host/aurora: sleep after 10mins

--- host/aurora.nix
+++ host/aurora.nix
@@ -18,6 +18,15 @@
     waybar.settings.mainBar.temperature.thermal-zone = 5;
   };
 
+  services.swayidle = {
+    timeouts = [
+      {
+        timeout = 600;
+        command = "/run/current-system/systemd/bin/systemctl suspend-then-hibernate";
+      }
+    ];
+  };
+
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (builtins.parseDrvName (lib.getName pkg)).name [
     "Anytype"
     "discord"
