fdcb570cf0ba2c4a2c93c2197aa3ec624bf5f684
Kira Bruneau <kira.bruneau@pm.me>
2023-11-09 16:13:43 -0500

    package/jellyfin: fix window assignment

--- package/jellyfin/default.nix
+++ package/jellyfin/default.nix
@@ -5,15 +5,12 @@
     jellyfin-media-player
   ];
 
-  wayland.windowManager.sway.config = {
-    assigns."4" = [{ app_id = "^org.jellyfin.$"; }];
-    window.commands = [
-      {
-        criteria = { app_id = "^org.jellyfin.$"; };
-        command = "inhibit_idle fullscreen";
-      }
-    ];
-  };
+  wayland.windowManager.sway.config.window.commands = [
+    {
+      criteria = { app_id = "^org.jellyfin.$"; };
+      command = "move container to workspace 4; inhibit_idle fullscreen";
+    }
+  ];
 
   xdg.dataFile."jellyfinmediaplayer/scripts/mpris.so".source =
     "${pkgs.mpvScripts.mpris}/share/mpv/scripts/mpris.so";
