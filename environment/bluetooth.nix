852c4fb9fd08889c04b8bdfc0e4027b9f87f8304
Kira Bruneau <kira.bruneau@pm.me>
2023-11-10 13:40:20 -0500

    environment/bluetooth: disable auto switching to headset profile

--- environment/bluetooth.nix
+++ environment/bluetooth.nix
@@ -1,4 +1,4 @@
-{ ... }:
+{ config, ... }:
 
 {
   hardware.bluetooth = {
@@ -10,4 +10,12 @@
 
   # ConfigurationDirectory 'bluetooth' already exists but the mode is different. (File system: 755 ConfigurationDirectoryMode: 555)
   systemd.services.bluetooth.serviceConfig.ConfigurationDirectoryMode = "0755";
+
+  # Disable auto switching to headset profile
+  environment.etc."wireplumber/policy.lua.d/11-bluetooth-policy.lua" = {
+    enable = config.services.pipewire.wireplumber.enable;
+    text = ''
+      bluetooth_policy.policy["media-role.use-headset-profile"] = false
+    '';
+  };
 }
