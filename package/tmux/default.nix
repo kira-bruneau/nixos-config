4b029c8ee112cf95bfabae7a6d30d5558e5d5aca
Kira Bruneau <kira.bruneau@pm.me>
2023-12-20 17:00:27 -0500

    package/tmux: fix attaching to existing session on ssh

--- package/tmux/default.nix
+++ package/tmux/default.nix
@@ -15,7 +15,7 @@ in
   # Automatically start tmux on SSH sessions
   programs.bash.profileExtra = lib.mkAfter ''
     if [ -z "$TMUX" ] && [ -n "$SSH_TTY" ]; then
-      exec ${tmux}/bin/tmux
+      exec ${tmux}/bin/tmux attach
     fi
   '';
 }
