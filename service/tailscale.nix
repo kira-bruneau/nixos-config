7be1b554b7a17515b467799a86362b8d5e19ef49
Kira Bruneau <kira.bruneau@pm.me>
2023-12-21 15:29:11 -0500

    service/tailscale: ignore tailscale0 for wait-online target

--- service/tailscale.nix
+++ service/tailscale.nix
@@ -1,11 +1,19 @@
+{ config, ... }:
+
 {
   services.tailscale = {
     enable = true;
     openFirewall = true;
   };
 
-  systemd.services.tailscaled.environment = {
-    TS_NO_LOGS_NO_SUPPORT = "true";
+  systemd = {
+    services.tailscaled.environment = {
+      TS_NO_LOGS_NO_SUPPORT = "true";
+    };
+
+    network.wait-online.ignoredInterfaces = [
+      config.services.tailscale.interfaceName
+    ];
   };
 
   networking.hosts = {
