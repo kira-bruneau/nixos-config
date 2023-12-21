f9f18948ff4358b6a7e54cdbb713c9677381adb1
Kira Bruneau <kira.bruneau@pm.me>
2023-12-21 15:15:10 -0500

    service/tailscale: disable logtail

--- service/tailscale.nix
+++ service/tailscale.nix
@@ -4,6 +4,10 @@
     openFirewall = true;
   };
 
+  systemd.services.tailscaled.environment = {
+    TS_NO_LOGS_NO_SUPPORT = "true";
+  };
+
   networking.hosts = {
     "100.64.0.1" = [ "quartz" ];
     "100.64.0.2" = [ "aurora" ];
