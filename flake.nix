6120d7527392f42b3201e4ab038a19c64ba9303e
Kira Bruneau <kira.bruneau@pm.me>
2023-12-22 14:25:02 -0500

    flake: use nixos-23.11 instead of release-23.11 for nixpkgs
    The release channel may contain packages that haven't been built by
    hydra. The nixos channel only gets bumped on a passing hydra run.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/release-23.11";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
