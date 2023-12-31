041ae215a89b6d8b3f7d1939f4eb89224cf1f4be
Kira Bruneau <kira.bruneau@pm.me>
2023-12-31 13:06:35 -0500

    package/gpg: don't directly share binary gpg key between machines

--- package/gpg/default.nix
+++ package/gpg/default.nix
@@ -4,13 +4,6 @@
   programs.gpg = {
     enable = true;
     homedir = "${config.xdg.dataHome}/gnupg";
-    publicKeys = [
-      {
-        # TODO: Refactor home-manager module so it doesn't put the source in the global nix store
-        source = config.lib.file.mkOutOfStoreSymlink
-          "${config.home.homeDirectory}/Auth/kira.asc";
-      }
-    ];
   };
 
   services.gpg-agent = {
