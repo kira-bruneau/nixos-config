59edc9a7237528dd151c6baf9bbe9b65b3844830
Kira Bruneau <kira.bruneau@pm.me>
2024-01-01 22:59:29 -0500

    home/environments/desktop/gnome: manually manage core utilities

--- environments/gui/gnome.nix
+++ environments/gui/gnome.nix
@@ -4,5 +4,6 @@
   ];
 
   services.xserver.desktopManager.gnome.enable = true;
+  services.gnome.core-utilities.enable = false;
   networking.networkmanager.enable = true;
 }
--- home/environments/gui/gnome.nix
+++ home/environments/gui/gnome.nix
@@ -1,3 +1,5 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./.
@@ -10,7 +12,23 @@
     ../../programs/gtk
   ];
 
-  home.packages = [
+  home.packages = with pkgs; [
+    # Administration
+    baobab
+    gnome.dconf-editor
+    gnome.gnome-disk-utility
+    gnome.gnome-system-monitor
+
+    # Media & Documents
+    gnome.file-roller
+    gnome.nautilus
+
+    # Utils
+    gnome-console
     gnome.gnome-clocks
+    gnome.seahorse
   ];
+
+  programs.bash.enableVteIntegration = true;
+  programs.zsh.enableVteIntegration = true;
 }
