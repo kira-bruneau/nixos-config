3bf178ec60041b6557abd687564c70caac04b181
Kira Bruneau <kira.bruneau@pm.me>
2024-01-04 19:33:10 -0500

    home/hosts/steamdeck: add dash-to-dock

--- home/hosts/steamdeck.nix
+++ home/hosts/steamdeck.nix
@@ -7,12 +7,40 @@
     ../environments/gui/gnome.nix
   ];
 
-  home.stateVersion = "23.11";
+  home = {
+    stateVersion = "23.11";
+    packages = with pkgs; [
+      gnomeExtensions.dash-to-dock
+    ];
+  };
 
   dconf.settings = {
     # Enable on-screen keyboard
     "org/gnome/desktop/a11y/applications" = {
       screen-keyboard-enabled = true;
+    };
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "dash-to-dock@micxgx.gmail.com"
+      ];
+    };
+    # Dash to Dock settings for a better touch screen experience
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      background-opacity = 0.80000000000000004;
+      custom-theme-shrink = true;
+      dash-max-icon-size = 48;
+      dock-fixed = true;
+      dock-position = "LEFT";
+      extend-height = true;
+      height-fraction = 0.60999999999999999;
+      hot-keys = false;
+      preferred-monitor = -2;
+      preferred-monitor-by-connector = "eDP-1";
+      scroll-to-focused-application = true;
+      show-apps-at-top = true;
+      show-mounts = true;
+      show-show-apps-button = true;
+      show-trash = false;
     };
   };
 
