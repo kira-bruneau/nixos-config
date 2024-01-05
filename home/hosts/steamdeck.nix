5b064c8a809fd29c047407975f54d43e5d036e35
Kira Bruneau <kira.bruneau@pm.me>
2024-01-04 19:31:58 -0500

    home/hosts/steamdeck: enable onscreen keyboard

--- home/hosts/steamdeck.nix
+++ home/hosts/steamdeck.nix
@@ -9,6 +9,13 @@
 
   home.stateVersion = "23.11";
 
+  dconf.settings = {
+    # Enable on-screen keyboard
+    "org/gnome/desktop/a11y/applications" = {
+      screen-keyboard-enabled = true;
+    };
+  };
+
   nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (builtins.parseDrvName (lib.getName pkg)).name [
     "clonehero"
     "clonehero-unwrapped"
