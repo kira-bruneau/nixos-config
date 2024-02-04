586ad1970f625238913a0b9e0eae048537340b52
Kira Bruneau <kira.bruneau@pm.me>
2024-02-03 23:59:07 -0500

    Revert "environment/gui: workaround bluman-applet not getting input"
    This reverts commit 8c74e8d52358e6218933bc9dc9c7fda39e44f78f.
    
    The workaround doesn't actually make a difference.

--- home/environments/bluetooth.nix
+++ home/environments/bluetooth.nix
@@ -1,12 +1,6 @@
 {
   services.blueman-applet.enable = true;
 
-  systemd.user.services.blueman-applet.Service.Environment = [
-    # Workaround bluman-applet not getting input
-    # https://bugreports.qt.io/browse/QTBUG-49952
-    "QT_XCB_NO_XI2_MOUSE=1"
-  ];
-
   # Forward bluetooth media controls to MPRIS
   services.mpris-proxy.enable = true;
 }
