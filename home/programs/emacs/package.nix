0491c56d88e86bbbebc7a088ca6684165939fa32
Kira Bruneau <kira.bruneau@pm.me>
2024-03-04 18:05:48 -0500

    home/programs/emacs: add tree-sitter grammar for tsx

--- home/programs/emacs/package.nix
+++ home/programs/emacs/package.nix
@@ -157,6 +157,7 @@ callPackage ./wrapper.nix {
       ts.tree-sitter-python
       ts.tree-sitter-ruby
       ts.tree-sitter-rust
+      ts.tree-sitter-tsx
       ts.tree-sitter-typescript
       ts.tree-sitter-yaml
     ]))
