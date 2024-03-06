025722b929205814cad9f315d8d57690cd96d186
Kira Bruneau <kira.bruneau@pm.me>
2024-03-06 11:58:40 -0500

    home/programs/emacs: use tree-sitter for c & c++ header files

--- home/programs/emacs/config/setup/fix-annoyances.el
+++ home/programs/emacs/config/setup/fix-annoyances.el
@@ -93,6 +93,7 @@
 (setq major-mode-remap-alist
  '((c++-mode . c++-ts-mode)
    (c-mode . c-ts-mode)
+   (c-or-c++-mode . c-or-c++-ts-mode)
    (cmake-mode . cmake-ts-mode)
    (css-mode . css-ts-mode)
    (java-mode . java-ts-mode)
