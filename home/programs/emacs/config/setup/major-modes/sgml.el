d79d9765cb92694906476975c97b83119b331739
Kira Bruneau <kira.bruneau@pm.me>
2024-03-06 12:12:55 -0500

    home/programs/emacs: fix sgml-electric-tag-pair-mode for tsx files

--- home/programs/emacs/config/setup/major-modes/sgml.el
+++ home/programs/emacs/config/setup/major-modes/sgml.el
@@ -1,4 +1,4 @@
 (use-package sgml-mode
   :hook ((web-mode . sgml-electric-tag-pair-mode)
          (js-ts-mode . sgml-electric-tag-pair-mode)
-         (jsx-ts-mode . sgml-electric-tag-pair-mode)))
+         (tsx-ts-mode . sgml-electric-tag-pair-mode)))
