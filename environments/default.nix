349a6a0efec6950384b84a5b5348e66fd0b46577
Kira Bruneau <kira.bruneau@pm.me>
2024-03-18 10:21:52 -0400

    flake.lock: update

--- environments/default.nix
+++ environments/default.nix
@@ -15,27 +15,21 @@
     useGlobalPkgs = true;
   };
 
-  nixpkgs.config = {
-    allowUnfreePredicate = pkg: builtins.elem (builtins.parseDrvName (lib.getName pkg)).name [
-      "anytype"
-      "anytype-heart"
-      "clonehero"
-      "clonehero-unwrapped"
-      "data.zip"
-      "discord"
-      "minecraft-server"
-      "sm64ex"
-      "steam"
-      "steam-jupiter-original"
-      "steam-original"
-      "steam-run"
-      "steamdeck-hw-theme"
-      "unrar"
-      "vvvvvv"
-    ];
-
-    permittedInsecurePackages = [
-      "nix-2.16.2" # Required by nixd
-    ];
-  };
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (builtins.parseDrvName (lib.getName pkg)).name [
+    "anytype"
+    "anytype-heart"
+    "clonehero"
+    "clonehero-unwrapped"
+    "data.zip"
+    "discord"
+    "minecraft-server"
+    "sm64ex"
+    "steam"
+    "steam-jupiter-original"
+    "steam-original"
+    "steam-run"
+    "steamdeck-hw-theme"
+    "unrar"
+    "vvvvvv"
+  ];
 }
