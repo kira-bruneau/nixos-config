f4878dcef7e4ad16db0d53d013e4dd4d41b15bcc
Kira Bruneau <kira.bruneau@pm.me>
2024-03-19 21:30:52 -0400

    users/kira: enable lingering

--- users/kira.nix
+++ users/kira.nix
@@ -22,6 +22,8 @@
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFolmVKlEFdALSIXtRNy/0ZqcTGn2H5/e3ieaIHoQr85"
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMIUYCcFr41Oy49T6v4296m/5bD2w/HgIubL3rf+3ULW"
     ];
+
+    linger = true;
   };
 
   home-manager.users.kira = ../home/hosts/${config.system.name}.nix;
