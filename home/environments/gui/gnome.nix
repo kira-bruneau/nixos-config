9bbac27430c7ee80118d5439c13e6abbae3fb620
Kira Bruneau <kira.bruneau@pm.me>
2024-03-23 16:55:43 -0400

    home/environment/gui/gnome: use dash-to-dock globally

--- home/environments/gui/gnome.nix
+++ home/environments/gui/gnome.nix
@@ -35,4 +35,30 @@
   xdg.mimeApps.defaultApplications = {
     "inode/directory" = "nautilus.desktop";
   };
+
+  dconf.settings = {
+    "org/gnome/shell" = {
+      enabled-extensions = [
+        "dash-to-dock@micxgx.gmail.com"
+      ];
+    };
+    # Dash to Dock settings for a better touch screen experience
+    "org/gnome/shell/extensions/dash-to-dock" = {
+      background-opacity = 0.80000000000000004;
+      custom-theme-shrink = true;
+      dash-max-icon-size = 48;
+      dock-fixed = true;
+      dock-position = "LEFT";
+      extend-height = true;
+      height-fraction = 0.60999999999999999;
+      hot-keys = false;
+      preferred-monitor = -2;
+      preferred-monitor-by-connector = "eDP-1";
+      scroll-to-focused-application = true;
+      show-apps-at-top = true;
+      show-mounts = true;
+      show-show-apps-button = true;
+      show-trash = false;
+    };
+  };
 }
