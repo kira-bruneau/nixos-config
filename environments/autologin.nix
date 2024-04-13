6d92e74b2a782b73530bcfaeb3306b12213ad870
Kira Bruneau <kira.bruneau@pm.me>
2024-04-13 15:59:21 -0400

    environments/stateless: move autologin config into separate file

---
+++ environments/autologin.nix
@@ -0,0 +1,11 @@
+{
+  services.greetd.settings.initial_session = {
+    command = "sway";
+    user = "kira";
+  };
+
+  services.xserver.displayManager.autoLogin = {
+    enable = true;
+    user = "kira";
+  };
+}
--- environments/stateless.nix
+++ environments/stateless.nix
@@ -1,18 +1,12 @@
 { lib, ... }:
 
 {
+  imports = [
+    ./autologin.nix
+  ];
+
   users.users.kira = {
     password = "kira";
     hashedPasswordFile = lib.mkForce null;
-  };
-
-  services.greetd.settings.initial_session = {
-    command = "sway";
-    user = "kira";
-  };
-
-  services.xserver.displayManager.autoLogin = {
-    enable = true;
-    user = "kira";
   };
 }
