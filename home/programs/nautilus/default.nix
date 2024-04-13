52f85f7e96cb0141bae7220f5a66ec68d90f4996
Kira Bruneau <kira.bruneau@pm.me>
2024-04-13 00:36:39 -0400

    home/programs/nautilus: decrease nautilus icon size

--- home/programs/nautilus/default.nix
+++ home/programs/nautilus/default.nix
@@ -5,6 +5,12 @@
     gnome.nautilus
   ];
 
+  dconf.settings = {
+    "org/gnome/nautilus/icon-view" = {
+      default-zoom-level = "small-plus";
+    };
+  };
+
   xdg.mimeApps.defaultApplications = lib.mkDefault {
     "inode/directory" = "nautilus.desktop";
   };
