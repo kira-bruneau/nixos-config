4ca2e529fd2cb6b9eb8f83ffdd076911a18735dc
Kira Bruneau <kira.bruneau@pm.me>
2024-04-13 00:05:30 -0400

    environments/gui/gnome: exclude gnome-tour & xterm

--- environments/gui/gnome.nix
+++ environments/gui/gnome.nix
@@ -1,9 +1,21 @@
+{ pkgs, ... }:
+
 {
   imports = [
     ./.
   ];
 
-  services.xserver.desktopManager.gnome.enable = true;
+  services.xserver = {
+    desktopManager.gnome.enable = true;
+    excludePackages = with pkgs; [
+      xterm
+    ];
+  };
+
   services.gnome.core-utilities.enable = false;
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-tour
+  ];
+
   networking.networkmanager.enable = true;
 }
