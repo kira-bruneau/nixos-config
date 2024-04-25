024fd4dde238d28b8600f6a7c14583aa99ecf065
Kira Bruneau <kira.bruneau@pm.me>
2024-04-25 18:31:34 -0400

    home/hosts/steamdeck: remove desktop entries for firefox kiosk apps

--- home/hosts/steamdeck.nix
+++ home/hosts/steamdeck.nix
@@ -343,30 +343,4 @@ in
       ];
     };
   };
-
-  xdg.desktopEntries = {
-    youtube = {
-      categories = [ "Network" "WebBrowser" ];
-      exec = "firefox --new-window --kiosk https://www.youtube.com";
-      genericName = "Web Browser";
-      icon = pkgs.fetchurl {
-        url = "https://upload.wikimedia.org/wikipedia/commons/0/09/YouTube_full-color_icon_%282017%29.svg";
-        hash = "sha256-fROAuewbDLM/ZhsgM9E77KfETCxAiabRTElVX/4/Ir8=";
-      };
-      name = "Youtube";
-      type = "Application";
-    };
-
-    dropout = {
-      categories = [ "Network" "WebBrowser" ];
-      exec = "firefox --new-window --kiosk https://www.dropout.tv";
-      genericName = "Web Browser";
-      icon = pkgs.fetchurl {
-        url = "https://theme.zdassets.com/theme_assets/2371800/512ccab6375a880e06985eb98aea5acbb9359ba7.jpg";
-        hash = "sha256-aSHPwfz5fhm5Ra9zJrK8Uxkz63UjIPl4nap3cWBAjIE=";
-      };
-      name = "Dropout";
-      type = "Application";
-    };
-  };
 }
