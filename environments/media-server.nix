5d6ae0f48c4a4df901e2b4fc0f00ffb52d6f01c2
Kira Bruneau <kira.bruneau@pm.me>
2024-05-04 14:15:41 -0400

    environments/media-server: prefer embedded titles for downloads

--- environments/media-server.nix
+++ environments/media-server.nix
@@ -138,7 +138,15 @@ let
         ];
       };
 
-      Downloads.folders = [ qBittorrent.BitTorrent."Session\\DefaultSavePath" ];
+      Downloads = {
+        folders = [ qBittorrent.BitTorrent."Session\\DefaultSavePath" ];
+        options = ''
+          <?xml version="1.0" encoding="utf-8"?>
+          <LibraryOptions xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+            <EnableEmbeddedTitles>true</EnableEmbeddedTitles>
+          </LibraryOptions>
+        '';
+      };
     };
   };
 
