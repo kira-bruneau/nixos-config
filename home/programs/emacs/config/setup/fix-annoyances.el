3c1cf19ed4d665312d151a3d18e5701d96ed911a
Kira Bruneau <kira.bruneau@pm.me>
2024-06-21 17:07:15 -0400

    home/programs/emacs: disable pager in term environment

--- home/programs/emacs/config/setup/fix-annoyances.el
+++ home/programs/emacs/config/setup/fix-annoyances.el
@@ -99,3 +99,11 @@
    (java-mode . java-ts-mode)
    (ruby-mode . ruby-ts-mode)
    (sh-mode . bash-ts-mode)))
+
+(defadvice compile (before ad-compile-smart activate)
+  "Advises `compile' so it sets the argument COMINT to t."
+  (ad-set-arg 1 t))
+
+(defadvice comint-term-environment (after disable-term-pager activate)
+  "Disable pager in term environment"
+  (setq ad-return-value (cons "PAGER=" ad-return-value)))
--- home/programs/emacs/config/setup/minor-modes/compile.el
+++
@@ -1,5 +0,0 @@
-(use-package compile
-  :config
-  (defadvice compile (before ad-compile-smart activate)
-    "Advises `compile' so it sets the argument COMINT to t."
-    (ad-set-arg 1 t)))
