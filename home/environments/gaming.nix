9ceedf4a4ee9e895e8d2269fdbc3cc8df30a71d2
Kira Bruneau <kira.bruneau@pm.me>
2024-07-15 14:32:15 -0400

    home/environments/gaming: update window class for steam

--- home/environments/gaming.nix
+++ home/environments/gaming.nix
@@ -57,7 +57,7 @@ in
     { app_id = "^org.dolphin-emu.$"; }
     { class = "^cemu.exe$"; }
     { class = "^dolphin-emu$"; }
-    { class = "^Steam$"; }
+    { class = "^steam$"; }
   ];
 
   home.file = {
