a8c449b8c0bf126d93ccfc5c769813678c155a42
Kira Bruneau <kira.bruneau@pm.me>
2024-07-19 18:52:05 -0400

    services/ollama: listenAddress -> host, port

--- services/ollama.nix
+++ services/ollama.nix
@@ -9,7 +9,8 @@
   services.ollama = {
     enable = true;
     package = pkgsUnstable.ollama;
-    listenAddress = "0.0.0.0:11434";
+    host = "0.0.0.0";
+    port = 11434;
     acceleration = "rocm";
     environmentVariables.OLLAMA_KEEP_ALIVE = "672h";
   };
