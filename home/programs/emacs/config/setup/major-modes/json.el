c942b39e0fc175e0480a0a730142e2709a633b30
Kira Bruneau <kira.bruneau@pm.me>
2024-07-20 21:16:17 -0400

    home/programs/emacs: use json-ts-mode for composer.lock files

--- home/programs/emacs/config/setup/major-modes/json.el
+++ home/programs/emacs/config/setup/major-modes/json.el
@@ -1,5 +1,5 @@
 (use-package json-ts-mode
-  :mode "\\(\\.\\(json\\|mcmeta\\)\\|/flake.lock\\)\\'"
+  :mode "\\(\\.\\(json\\|mcmeta\\)\\|/\\(flake.lock\\|composer.lock\\)\\)\\'"
   :init
   (with-eval-after-load 'restclient
     (add-to-list 'restclient-content-type-modes '("application/json" . json-ts-mode))))
