145311b783ad2453f981ff44c66a9b5b68fa2401
Kira Bruneau <kira.bruneau@pm.me>
2024-07-20 21:09:27 -0400

    home/programs/emacs: use json-ts-mode for flake.lock files

--- home/programs/emacs/config/setup/major-modes/json.el
+++ home/programs/emacs/config/setup/major-modes/json.el
@@ -1,5 +1,5 @@
 (use-package json-ts-mode
-  :mode "\\.\\(json\\|mcmeta\\)\\'"
+  :mode "\\(\\.\\(json\\|mcmeta\\)\\|/flake.lock\\)\\'"
   :init
   (with-eval-after-load 'restclient
     (add-to-list 'restclient-content-type-modes '("application/json" . json-ts-mode))))
