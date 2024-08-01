8fc8e4870a4e3f0d89a29ba14fb5be778692b205
Kira Bruneau <kira.bruneau@pm.me>
2024-08-01 00:36:38 -0400

    flake: use nixos-25.11

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       inputs.flake-utils.follows = "flake-utils";
     };
 
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
 
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
@@ -28,7 +28,7 @@
     impermanence.url = "github:nix-community/impermanence";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
