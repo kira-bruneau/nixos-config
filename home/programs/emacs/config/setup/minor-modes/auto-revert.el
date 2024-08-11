7e4252ae34a41a8ac68db48b1361c6d981094acd
Kira Bruneau <kira.bruneau@pm.me>
2024-08-11 14:28:12 -0400

    home/programs/emacs: fix running auto-revert-mode in dired
    When auto-revert-avoid-polling is enabled, dired won't be considered
    for global-auto-revert-mode, so we have to explicitly enable it.

--- home/programs/emacs/config/setup/minor-modes/auto-revert.el
+++ home/programs/emacs/config/setup/minor-modes/auto-revert.el
@@ -1,5 +1,6 @@
 (use-package autorevert
   :bind (("<f5>" . revert-buffer))
+  :hook (dired-mode . auto-revert-mode)
   :config
   (setq auto-revert-verbose nil)
   (setq auto-revert-avoid-polling t)
