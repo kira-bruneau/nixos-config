9e0159417459742dfe511bf19750c49e56e4068e
Kira Bruneau <kira.bruneau@pm.me>
2024-08-17 17:28:24 -0400

    home/programs/emacs: use consult-buffer for all buffer commands

--- home/programs/emacs/config/setup/modules/consult.el
+++ home/programs/emacs/config/setup/modules/consult.el
@@ -1,6 +1,14 @@
 (use-package consult
-  :bind (("C-s" . consult-line)
+  :bind (;; Search
+         ("C-s" . consult-line)
          ("C-r" . nil)
+
+         ;; Buffer switching
+         ([remap switch-to-buffer] . consult-buffer)
+         ([remap switch-to-buffer-other-frame] . consult-buffer-other-frame)
+         ([remap switch-to-buffer-other-window] . consult-buffer-other-window)
+         ([remap switch-to-buffer-other-tab] . consult-buffer-other-tab)
+         ([remap project-switch-to-buffer] . consult-project-buffer)
 
          :map minibuffer-local-map
          ("C-r" . consult-history))
