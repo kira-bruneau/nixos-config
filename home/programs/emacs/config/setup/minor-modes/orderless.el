bd8f21d10d8a0f967f1ad7501d18233f374441d5
Kira Bruneau <kira.bruneau@pm.me>
2024-08-21 17:25:47 -0400

    home/programs/emacs: fix orderless-matching-styles

--- home/programs/emacs/config/setup/minor-modes/orderless.el
+++ home/programs/emacs/config/setup/minor-modes/orderless.el
@@ -3,4 +3,7 @@
   (completion-styles '(orderless basic))
   (completion-category-defaults nil)
   (completion-category-overrides '((file (styles partial-completion))))
-  (setq orderless-matching-styles '(orderless-literal orderless-regexp orderless-prefixes)))
+  (orderless-matching-styles
+   '(orderless-literal
+     orderless-regexp
+     orderless-prefixes)))
