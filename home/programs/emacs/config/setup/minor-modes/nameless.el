095c45bb7026e611e8840d9c759f8db67fa4c0a6
Kira Bruneau <kira.bruneau@pm.me>
2024-08-21 16:19:13 -0400

    home/programs/emacs: run nameless in consult preview

--- home/programs/emacs/config/setup/minor-modes/nameless.el
+++ home/programs/emacs/config/setup/minor-modes/nameless.el
@@ -1,2 +1,5 @@
 (use-package nameless
-  :hook emacs-lisp-mode)
+  :hook ((emacs-lisp-mode . nameless-mode)
+         (consult-after-jump . (lambda ()
+                                 (when (derived-mode-p 'emacs-lisp-mode)
+                                   (nameless-mode))))))
