0336427272e626e4f9c31a2a01230c9008697120
Kira Bruneau <kira.bruneau@pm.me>
2024-08-21 11:09:33 -0400

    home/programs/emacs: add orderless-prefixes style

--- home/programs/emacs/config/setup/minor-modes/orderless.el
+++ home/programs/emacs/config/setup/minor-modes/orderless.el
@@ -2,4 +2,5 @@
   :custom
   (completion-styles '(orderless basic))
   (completion-category-defaults nil)
-  (completion-category-overrides '((file (styles partial-completion)))))
+  (completion-category-overrides '((file (styles partial-completion))))
+  (setq orderless-matching-styles '(orderless-literal orderless-regexp orderless-prefixes)))
