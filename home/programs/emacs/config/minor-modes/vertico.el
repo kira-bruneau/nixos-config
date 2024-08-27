a03f813b1c5cfad22760d43f5eefac63759bdfde
Kira Bruneau <kira.bruneau@pm.me>
2024-08-26 22:18:58 -0400

    home/programs/emacs: use more convenient vertico-directory bindings

--- home/programs/emacs/config/minor-modes/vertico.el
+++ home/programs/emacs/config/minor-modes/vertico.el
@@ -2,3 +2,8 @@
   :demand
   :config
   (vertico-mode))
+
+(use-package vertico-directory
+  :bind (:map vertico-map
+         ("DEL" . vertico-directory-delete-char)
+         ("M-DEL" . vertico-directory-delete-word)))
