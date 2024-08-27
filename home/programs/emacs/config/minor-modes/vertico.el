9e37cebf30fde2905e2980ea5d21a70c8a8ea609
Kira Bruneau <kira.bruneau@pm.me>
2024-08-27 12:06:31 -0400

    home/programs/emacs: implement counsel style '~' & '/' expansion

--- home/programs/emacs/config/minor-modes/vertico.el
+++ home/programs/emacs/config/minor-modes/vertico.el
@@ -5,6 +5,21 @@
 
 (use-package vertico-directory
   :bind (:map vertico-map
+         ("/" . vertico-directory-insert)
+         ("~" . vertico-directory-home)
          ("DEL" . vertico-directory-delete-char)
          ("M-DEL" . vertico-directory-delete-word))
-  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy))
+  :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
+  :config
+  (defun vertico-directory-insert (n &optional c)
+    (interactive "p")
+    (if (and (eq 'file (vertico--metadata-get 'category))
+             (eq n 1)
+             (eq (point) (point-max))
+             (not (eq (char-before) ?/)))
+        (vertico-insert)
+      (self-insert-command n c)))
+
+  (defun vertico-directory-home ()
+    (interactive)
+    (insert "~/")))
