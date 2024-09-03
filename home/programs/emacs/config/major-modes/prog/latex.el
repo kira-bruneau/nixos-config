76d1b9f69b3db11590e514ed4b34d7c2fa2dd8eb
Kira Bruneau <kira.bruneau@pm.me>
2024-09-03 11:51:43 -0400

    home/programs/emacs: fix lpp/window-containing-preview

--- home/programs/emacs/config/major-modes/prog/latex.el
+++ home/programs/emacs/config/major-modes/prog/latex.el
@@ -1,3 +1,13 @@
 (use-package tex-mode
   :bind (:map latex-mode-map
               ("M-p" . latex-preview-pane-mode)))
+
+(use-package latex-preview-pane
+  :config
+  ;; Fix lpp/window-containing-preview
+  ;; https://github.com/jsinglet/latex-preview-pane/pull/58
+  (defun lpp/window-containing-preview ()
+    (let (windows i docViewWindow)
+      (let ((windows (cl-reduce #'append (mapcar `window-list (frame-list)))))
+        (setq windows (cl-reduce #'append (mapcar `window-list (frame-list))))
+        (cl-find-if (lambda (window) (window-parameter window 'is-latex-preview-pane)) windows)))))
