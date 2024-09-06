6d547ac14691b88a476a7ff026dc11decefe3e3f
Kira Bruneau <kira.bruneau@pm.me>
2024-09-06 10:16:28 -0400

    environments/home-assistant: add waves theme

--- environments/home-assistant.nix
+++ environments/home-assistant.nix
@@ -1,15 +1,34 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
+let
+  waves = pkgs.fetchFromGitHub {
+    owner = "tgcowell";
+    repo = "waves";
+    rev = "refs/tags/v2.4.1";
+    hash = "sha256-koUOdwt9t6iOQn4T/voV4Hvc5moqnf/PkXFp/nuFytM=";
+  };
+in
 {
   services.home-assistant = {
     enable = true;
     extraComponents = [ "vesync" ];
     extraPackages = ps: with ps; [ numpy ];
-    config.homeassistant = {
-      name = "Home";
-      latitude = config.location.latitude;
-      longitude = config.location.longitude;
-      unit_system = "metric";
+    config = {
+      homeassistant = {
+        name = "Home";
+        latitude = config.location.latitude;
+        longitude = config.location.longitude;
+        unit_system = "metric";
+      };
+
+      frontend = {
+        themes = "!include ${waves}/themes/waves.yaml";
+      };
     };
   };
+
+  systemd.services.home-assistant.preStart = ''
+    mkdir -p ${config.services.home-assistant.configDir}/www
+    ln -fns ${waves}/themes ${config.services.home-assistant.configDir}/www/waves
+  '';
 }
