24826776f8e5910e78fa7077509b57651d3e262c
Kira Bruneau <kira.bruneau@pm.me>
2024-09-05 20:56:09 -0400

    home/programs/emacs: use dired-hide-details-mode

--- home/programs/emacs/config/major-modes/dired.el
+++ home/programs/emacs/config/major-modes/dired.el
@@ -1,6 +1,7 @@
 (use-package dired
   :bind (:map dired-mode-map
               ("I" . image-dired-default-directory))
+  :hook (dired-mode . dired-hide-details-mode)
   :config
   (setq dired-dwim-target t)
   (setq dired-listing-switches "-alh")
