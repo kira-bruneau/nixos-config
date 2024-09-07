789c86355a9614bae88c320e10d6361d7b415625
Kira Bruneau <kira.bruneau@pm.me>
2024-09-07 10:32:28 -0400

    home/environments/nix-gc: use persistent module option

--- home/environments/nix-gc.nix
+++ home/environments/nix-gc.nix
@@ -3,8 +3,6 @@
     automatic = true;
     frequency = "weekly";
     options = "--delete-older-than 30d";
+    persistent = true;
   };
-
-  # TODO: Use module option once 24.11 is released
-  systemd.user.timers.nix-gc.Timer.Persistent = true;
 }
