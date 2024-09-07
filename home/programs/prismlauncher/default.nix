30b24b68a7c5adb4a746b9941cd4ca2319ff6df3
Kira Bruneau <kira.bruneau@pm.me>
2024-09-07 10:30:02 -0400

    home/programs/prismlauncher: withWaylandGLFW option was removed
    https://github.com/NixOS/nixpkgs/pull/310073/files

--- home/programs/prismlauncher/default.nix
+++ home/programs/prismlauncher/default.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ (prismlauncher.override { withWaylandGLFW = true; }) ];
+  home.packages = with pkgs; [ prismlauncher ];
 
   wayland.windowManager.sway.config.assigns."5" = [
     { app_id = "^org.prismlauncher.PrismLauncher$"; }
