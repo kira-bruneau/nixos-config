e83f8075299890b61257b1352c495aef036b3756
Kira Bruneau <kira.bruneau@pm.me>
2024-09-17 17:39:00 -0400

    home/programs/emacs: use zero to disable modes

--- home/programs/emacs/config/modules/ui.el
+++ home/programs/emacs/config/modules/ui.el
@@ -1,7 +1,7 @@
 ;; GUI cleanup
-(scroll-bar-mode -1)
-(menu-bar-mode -1)
-(tool-bar-mode -1)
+(scroll-bar-mode 0)
+(menu-bar-mode 0)
+(tool-bar-mode 0)
 (setq inhibit-startup-screen t)
 (setq initial-major-mode 'fundamental-mode)
 (setq initial-scratch-message nil)
