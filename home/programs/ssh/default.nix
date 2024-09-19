309e49ca46630213b769bd40f991566a573e0294
Kira Bruneau <kira.bruneau@pm.me>
2024-09-19 19:20:45 -0400

    home/programs/ssh: set connect timeout

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -8,6 +8,10 @@
     controlMaster = "auto";
     controlPersist = "5m";
 
+    extraConfig = ''
+      ConnectTimeout 3
+    '';
+
     # Manage known_hosts outside of home-manager (synced with Syncthing)
     userKnownHostsFile = builtins.toString (
       config.lib.file.mkOutOfStoreSymlink "${config.home.configDirectory}/programs/ssh/known_hosts"
