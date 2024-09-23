8f1e87100747d52ebe6a959e66133e7f065090cc
Kira Bruneau <kira.bruneau@pm.me>
2024-09-23 12:25:34 -0400

    home/programs/emacs: remove partial completion for files

--- home/programs/emacs/config/minor-modes/orderless.el
+++ home/programs/emacs/config/minor-modes/orderless.el
@@ -3,7 +3,6 @@
   (completion-ignored-extensions nil)
   (completion-styles '(orderless basic))
   (completion-category-defaults nil)
-  (completion-category-overrides '((file (styles partial-completion))))
   (orderless-matching-styles
    '(orderless-literal
      orderless-regexp
