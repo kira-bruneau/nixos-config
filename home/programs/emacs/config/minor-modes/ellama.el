ecf70981df79c8ff253c6a1190e86d542a518a44
Kira Bruneau <kira.bruneau@pm.me>
2024-10-18 22:21:08 -0400

    home/programs/emacs: use custom major mode for ellama buffers

--- home/programs/emacs/config/minor-modes/ellama.el
+++ home/programs/emacs/config/minor-modes/ellama.el
@@ -1,7 +1,14 @@
 (use-package ellama
+  :custom
+  (ellama-major-mode 'ellama-mode)
+
   :config
   (setopt ellama-provider
           (make-llm-ollama
            :host "quartz"
            :chat-model "deepseek-coder-v2:16b-lite-instruct-q8_0"
-           :embedding-model "deepseek-coder-v2:16b-lite-instruct-q8_0")))
+           :embedding-model "deepseek-coder-v2:16b-lite-instruct-q8_0"))
+
+  (define-derived-mode ellama-mode org-mode "Ellama"
+    "Major mode for Ellama buffers"
+    (org-indent-mode 0)))
