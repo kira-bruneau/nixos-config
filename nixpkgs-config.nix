d211113f6f626e2f95ea8d89f839d982cc69aae3
Kira Bruneau <kira.bruneau@pm.me>
2024-12-08 21:04:06 +0000

    nixpkgs-config: .NET 6 exceptions only rely on sonarr now

--- nixpkgs-config.nix
+++ nixpkgs-config.nix
@@ -28,10 +28,9 @@
     ];
 
   permittedInsecurePackages = [
-    # omnisharp-roslyn
+    # sonarr: https://github.com/NixOS/nixpkgs/issues/360592
     "aspnetcore-runtime-6.0.36"
     "aspnetcore-runtime-wrapped-6.0.36"
-    "dotnet-core-combined"
     "dotnet-sdk-6.0.428"
     "dotnet-sdk-wrapped-6.0.428"
 
