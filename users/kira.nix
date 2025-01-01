9302ca9f2051669a2247886c8f900a7f74211668
Kira Bruneau <kira.bruneau@pm.me>
2024-12-31 20:46:39 -0500

    users/kira: only specify ssh user when different from kira

--- users/kira.nix
+++ users/kira.nix
@@ -108,17 +108,8 @@
       };
 
       ssh.matchBlocks = {
-        "amethyst".user = "kira";
-        "amethyst.lan".user = "kira";
-        "aurora".user = "kira";
-        "aurora.lan".user = "kira";
         "jackflix".user = "jakira";
         "jackflix.lan".user = "jakira";
-        "jakira.space".user = "kira";
-        "quartz".user = "kira";
-        "quartz.lan".user = "kira";
-        "gneiss".user = "kira";
-        "gneiss.lan".user = "kira";
         "steamdeck".user = "jakira";
         "steamdeck.lan".user = "jakira";
       };
