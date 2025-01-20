bb12d23a86095d74153569037b1221335b9d40e4
Kira Bruneau <kira.bruneau@pm.me>
2025-01-20 11:54:27 -0500

    hardware/hosts: cleanup nix build machine config

--- hardware/hosts/gneiss/shared.nix
+++ hardware/hosts/gneiss/shared.nix
@@ -1,17 +1,15 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   nix = {
-    buildMachines = builtins.filter (machine: machine.hostName != config.networking.hostName) [
-      {
-        protocol = "ssh-ng";
-        sshUser = "builder";
-        hostName = "gneiss";
-        maxJobs = 4; # 4 cores
-        speedFactor = 1296; # MHz, max "boost" clock speed
-        systems = [ "aarch64-linux" ];
-      }
-    ];
+    buildMachines = lib.optional (config.networking.hostName != "gneiss") {
+      protocol = "ssh-ng";
+      sshUser = "builder";
+      hostName = "gneiss";
+      maxJobs = 4; # 4 cores
+      speedFactor = 1296; # MHz, max "boost" clock speed
+      systems = [ "aarch64-linux" ];
+    };
 
     settings.trusted-public-keys = [
       "rocky:JuZH5XWnnaZj03DtceTDe5xEHHMgcn0+vzToW1Z3Pgg="
@@ -20,5 +18,6 @@
 
   networking.hosts."100.64.0.10" = [ "gneiss" ];
 
-  programs.ssh.knownHosts.gneiss.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUGzRm127zLH3ZQxwGTFmWyggeu7uwoaMnCGUdCq0fO";
+  programs.ssh.knownHosts.gneiss.publicKey =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINUGzRm127zLH3ZQxwGTFmWyggeu7uwoaMnCGUdCq0fO";
 }
--- hardware/hosts/quartz/shared.nix
+++ hardware/hosts/quartz/shared.nix
@@ -1,27 +1,25 @@
-{ config, ... }:
+{ config, lib, ... }:
 
 {
   nix = {
-    buildMachines = builtins.filter (machine: machine.hostName != config.networking.hostName) [
-      {
-        protocol = "ssh-ng";
-        sshUser = "builder";
-        hostName = "quartz";
+    buildMachines = lib.optional (config.networking.hostName != "quartz") {
+      protocol = "ssh-ng";
+      sshUser = "builder";
+      hostName = "quartz";
 
-        maxJobs = 12; # 6 cores, each with 2 threads
-        speedFactor = 3400; # MHz, max "boost" clock speed
+      maxJobs = 12; # 6 cores, each with 2 threads
+      speedFactor = 3400; # MHz, max "boost" clock speed
 
-        systems = [
-          "x86_64-linux"
-          "i686-linux"
-        ];
+      systems = [
+        "x86_64-linux"
+        "i686-linux"
+      ];
 
-        supportedFeatures = [
-          "big-parallel"
-          "kvm"
-        ];
-      }
-    ];
+      supportedFeatures = [
+        "big-parallel"
+        "kvm"
+      ];
+    };
 
     settings.trusted-public-keys = [
       "quartz:5ihtRHWq3L8mirx1UEy2uDAkb12NQUN+t+OT4NAnEp8="
@@ -30,7 +28,8 @@
 
   networking.hosts."100.64.0.1" = [ "quartz" ];
 
-  programs.ssh.knownHosts.quartz.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPBPD66/axLTeJHQ+lLmOSJT2VQyESnk5VRr7Rkx4BET";
+  programs.ssh.knownHosts.quartz.publicKey =
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPBPD66/axLTeJHQ+lLmOSJT2VQyESnk5VRr7Rkx4BET";
 
   services.syncthing.settings = {
     devices.quartz.id = "64ZDVRR-2DZB475-3IWMGU6-OU46FZQ-P44AXVI-OYI6TO3-VOCUVRT-L62KBAE";
