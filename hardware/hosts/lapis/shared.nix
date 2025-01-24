1f1fe3247cd2bde6eecf174727a1a7b36cfbfa2d
Kira Bruneau <kira.bruneau@pm.me>
2025-01-24 16:47:59 -0500

    hardware/hosts/aurora: rename to lapis

---
+++ hardware/hosts/lapis/shared.nix
@@ -0,0 +1,20 @@
+{
+  nix.settings.trusted-public-keys = [ "aurora:PkeJpeCTFE3gprtNpxCW0EqbVwg0wFgvpHFq3Hj0Wlc=" ];
+
+  networking.hosts."100.64.0.2" = [ "lapis" ];
+
+  programs.ssh.knownHosts.lapis.publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDG2wKEtIS2KuoXr4uNAzwTLvkyjrLX9zonE3pZB2pdH";
+
+  services.syncthing.settings = {
+    devices.lapis.id = "ODCDVEV-I63ZAW6-MV27YBB-W5MDOAU-YZ3RK23-DMXCWAN-STJOSEF-EFXFRQP";
+
+    folders = {
+      "Auth".devices = [ "lapis" ];
+      "Dev".devices = [ "lapis" ];
+      "Documents".devices = [ "lapis" ];
+      "Pictures".devices = [ "lapis" ];
+      "RSS".devices = [ "lapis" ];
+      "Videos".devices = [ "lapis" ];
+    };
+  };
+}
