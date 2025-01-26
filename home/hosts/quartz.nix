2664bf0561d84aebfe95ca549233a65d8828e517
Kira Bruneau <kira.bruneau@pm.me>
2025-01-25 19:50:03 -0500

    home/hosts/quartz: auto build nixos & sync nur-packages daily

--- home/hosts/quartz.nix
+++ home/hosts/quartz.nix
@@ -1,4 +1,11 @@
 {
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
   imports = [
     ../environments/art.nix
     ../environments/bluetooth.nix
@@ -13,5 +20,53 @@
   programs.waybar.settings.mainBar.temperature = {
     hwmon-path-abs = "/sys/devices/platform/asus-ec-sensors/hwmon";
     input-filename = "temp2_input";
+  };
+
+  systemd.user = {
+    services = {
+      update-nixos-main = {
+        Unit = {
+          Wants = [ "update-nur.service" ];
+          After = [ "update-nur.service" ];
+        };
+
+        Service = {
+          Type = "oneshot";
+          WorkingDirectory = "${config.home.homeDirectory}/Dev/public/nixos-config/main";
+          ExecStart = pkgs.writeShellScript "update-nixos" ''
+            export PATH=${
+              lib.makeBinPath (
+                with pkgs;
+                [
+                  nix
+                  nixos-rebuild
+                  git
+                ]
+              )
+            }
+
+            nix flake update --commit-lock-file --option commit-lockfile-summary "flake.lock: update"
+            nixos-rebuild build --flake .
+          '';
+        };
+      };
+
+      update-nur = {
+        Service = {
+          Type = "oneshot";
+          WorkingDirectory = "${config.home.homeDirectory}/Dev/public/nur-packages";
+          ExecStart = pkgs.writeShellScript "update-nur" ''
+            export PATH=${lib.makeBinPath (with pkgs; [ nix ])}
+            nix run .#update
+            nix run .#sync ~/Dev/nixpkgs/nur
+          '';
+        };
+      };
+    };
+
+    timers.update-nixos-main = {
+      Timer.OnCalendar = "daily";
+      Install.WantedBy = [ "timers.target" ];
+    };
   };
 }
