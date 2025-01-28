aef8415d29bdd66f95ee571d779cafc339374df9
Kira Bruneau <kira.bruneau@pm.me>
2025-01-28 11:19:29 -0500

    home/hosts/quartz: auto build peridot weekly

--- home/hosts/quartz.nix
+++ home/hosts/quartz.nix
@@ -33,7 +33,7 @@
         Service = {
           Type = "oneshot";
           WorkingDirectory = "${config.home.homeDirectory}/Dev/public/nixos-config/main";
-          ExecStart = pkgs.writeShellScript "update-nixos" ''
+          ExecStart = pkgs.writeShellScript "update-nixos-main" ''
             export PATH=${
               lib.makeBinPath (
                 with pkgs;
@@ -51,6 +51,63 @@
         };
       };
 
+      update-nixos-unstable = {
+        Unit = {
+          Wants = [ "update-nixos-main.service" ];
+          After = [ "update-nixos-main.service" ];
+        };
+
+        Service = {
+          Type = "oneshot";
+          WorkingDirectory = "${config.home.homeDirectory}/Dev/public/nixos-config/unstable";
+          ExecStart = pkgs.writeShellScript "update-nixos-unstable" ''
+            export PATH=${
+              lib.makeBinPath (
+                with pkgs;
+                [
+                  nix
+                  git
+                ]
+              )
+            }
+
+            if ! git pull --rebase; then
+              rm flake.lock
+              nix flake lock
+              git add flake.lock
+              GIT_EDITOR=true git rebase --continue
+            fi
+          '';
+        };
+      };
+
+      update-nixos-peridot = {
+        Unit = {
+          Requires = [ "update-nixos-unstable.service" ];
+          After = [ "update-nixos-unstable.service" ];
+        };
+
+        Service = {
+          Type = "oneshot";
+          WorkingDirectory = "${config.home.homeDirectory}/Dev/public/nixos-config/peridot";
+          ExecStart = pkgs.writeShellScript "update-nixos-peridot" ''
+            export PATH=${
+              lib.makeBinPath (
+                with pkgs;
+                [
+                  git
+                  nix
+                  nixos-rebuild
+                ]
+              )
+            }
+
+            git pull --rebase
+            nixos-rebuild build --flake .#peridot
+          '';
+        };
+      };
+
       update-nur = {
         Service = {
           Type = "oneshot";
@@ -63,9 +120,16 @@
       };
     };
 
-    timers.update-nixos-main = {
-      Timer.OnCalendar = "daily";
-      Install.WantedBy = [ "timers.target" ];
+    timers = {
+      update-nixos-main = {
+        Timer.OnCalendar = "daily";
+        Install.WantedBy = [ "timers.target" ];
+      };
+
+      update-nixos-peridot = {
+        Timer.OnCalendar = "weekly";
+        Install.WantedBy = [ "timers.target" ];
+      };
     };
   };
 }
