ff041e501f0940813a665f1fcb9119d7525ea464
Kira Bruneau <kira.bruneau@pm.me>
2025-06-13 16:16:03 -0400

    home/programs/librewolf: move teams to chat workspace

--- home/programs/librewolf/default.nix
+++ home/programs/librewolf/default.nix
@@ -827,6 +827,14 @@
       {
         criteria = {
           app_id = "^librewolf$";
+          title = "https://teams.microsoft.com";
+        };
+
+        command = "move container to workspace 10";
+      }
+      {
+        criteria = {
+          app_id = "^librewolf$";
           title = "^Picture-in-Picture$";
         };
 
