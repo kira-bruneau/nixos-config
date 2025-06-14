43ccdc94a2bc3afd4d0cf30f433f16b9355369c4
Kira Bruneau <kira.bruneau@pm.me>
2025-06-14 09:50:10 -0400

    hosts/sapphire: disable greetd

--- hosts/sapphire.nix
+++ hosts/sapphire.nix
@@ -1,3 +1,5 @@
+{ lib, ... }:
+
 {
   imports = [
     ../environments/autologin.nix
@@ -9,4 +11,12 @@
   system.stateVersion = "25.05";
 
   users.defaultUser = "kira";
+
+  services.greetd.enable = lib.mkForce false;
+
+  environment.loginShellInit = ''
+    if [ -z "$WAYLAND_DISPLAY" ] && [ -n "$XDG_VTNR" ] && [ "$XDG_VTNR" -eq 1 ]; then
+      exec sway
+    fi
+  '';
 }
