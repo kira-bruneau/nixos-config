e7bec0b2199c45a42a7a98f6eac531dfe84dac95
Kira Bruneau <kira.bruneau@pm.me>
2025-07-28 19:42:49 -0400

    modules: remove mautrix-discord

--- services/mautrix-discord.nix
+++ services/mautrix-discord.nix
@@ -1,10 +1,10 @@
-{ config, ... }:
+{ lib, config, ... }:
 
 {
   services.mautrix-discord = {
     enable = true;
 
-    settings = {
+    settings = lib.mkForce {
       homeserver = {
         address = "http://localhost:8008";
         domain = config.services.matrix-synapse.settings.server_name;
@@ -18,11 +18,37 @@
         };
       };
 
-      bridge.permissions = {
-        "*" = "relay";
-        "jakira.space" = "user";
-        "@jack:jakira.space" = "admin";
-        "@kira:jakira.space" = "admin";
+      bridge = {
+        permissions = {
+          "*" = "relay";
+          "jakira.space" = "user";
+          "@jack:jakira.space" = "admin";
+          "@kira:jakira.space" = "admin";
+        };
+
+        encryption = {
+          allow = true;
+          default = true;
+          require = true;
+
+          # Recommended options from mautrix documentation
+          # for additional security.
+          delete_keys = {
+            dont_store_outbound = true;
+            ratchet_on_decrypt = true;
+            delete_fully_used_on_decrypt = true;
+            delete_prev_on_new_session = true;
+            delete_on_device_delete = true;
+            periodically_delete_expired = true;
+            delete_outdated_inbound = true;
+          };
+
+          verification_levels = {
+            receive = "cross-signed-tofu";
+            send = "cross-signed-tofu";
+            share = "cross-signed-tofu";
+          };
+        };
       };
 
       logging.writers = [
