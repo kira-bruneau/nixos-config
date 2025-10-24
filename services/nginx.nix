4c6a5ba3f7a8b3e124cf78e9b660d6274c7b2bb0
Kira Bruneau <kira.bruneau@pm.me>
2025-10-23 23:43:10 -0400

    services/nginx: log to systemd journal

--- services/nginx.nix
+++ services/nginx.nix
@@ -5,5 +5,13 @@
     recommendedOptimisation = true;
     recommendedGzipSettings = true;
     recommendedProxySettings = true;
+    commonHttpConfig = ''
+      log_format systemd '$remote_addr - $remote_user '
+                         '"$request" $status $body_bytes_sent '
+                         '"$http_referer" "$http_user_agent"';
+
+      access_log syslog:server=unix:/dev/log systemd;
+      error_log syslog:server=unix:/dev/log;
+    '';
   };
 }
