59d68777df677b93b80731a6bd41b7b6884b1feb
Kira Bruneau <kira.bruneau@pm.me>
2025-11-02 15:48:39 -0500

    hardware/hosts/jasper: configure wireguard

--- hardware/hosts/jasper/shared.nix
+++ hardware/hosts/jasper/shared.nix
@@ -1,5 +1,19 @@
 { config, lib, ... }:
 
 {
-  networking.hosts."100.64.0.15" = lib.mkIf config.services.tailscale.enable [ "jasper" ];
+  networking = {
+    wireguard.interfaces.wg0 = {
+      ips = lib.optional (config.networking.hostName == "jasper") "10.100.0.4/32";
+      peers = lib.optional (config.networking.hostName != "jasper") {
+        name = "jasper";
+        publicKey = "+g5erg0fSKLuLMD2UiVeLpW3RFlWLoVt03En8oRc+A8=";
+        allowedIPs = [ "10.100.0.4/32" ];
+      };
+    };
+
+    hosts = {
+      "100.64.0.15" = lib.mkIf config.services.tailscale.enable [ "jasper" ];
+      "10.100.0.4" = lib.mkIf config.networking.wireguard.enable [ "jasper" ];
+    };
+  };
 }
