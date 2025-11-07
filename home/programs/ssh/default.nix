9b364dab6eb23a40470bbf28c5d764707092df9d
Kira Bruneau <kira.bruneau@pm.me>
2025-11-06 21:31:34 -0500

    home/programs/ssh: disable default ssh config

--- home/programs/ssh/default.nix
+++ home/programs/ssh/default.nix
@@ -3,10 +3,12 @@
 {
   programs.ssh = {
     enable = true;
+    enableDefaultConfig = false;
 
     matchBlocks."*" = {
       # Share SSH connections
       controlMaster = "auto";
+      controlPath = "~/.ssh/master-%r@%n:%p";
       controlPersist = "5m";
 
       # Manage known_hosts outside of home-manager (synced with Syncthing)
