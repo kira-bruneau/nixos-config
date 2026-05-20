c8917533ddf024bf279dfa95ffca36ce0717d783
Kira Bruneau <kira.bruneau@pm.me>
2026-05-20 08:47:50 -0400

    home/programs/emacs: fix language-specific apheleia formatting

--- home/programs/emacs/config/minor-modes/apheleia.el
+++ home/programs/emacs/config/minor-modes/apheleia.el
@@ -1,3 +1,57 @@
 (use-package apheleia
   :config
-  (add-to-list 'apheleia-formatters '(prettier . ("prettierd" filepath))))
+  (setq
+   apheleia-formatters
+   (append
+    '((prettier
+       . ("prettierd" "--stdin-filepath" filepath
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-css
+       . ("prettierd" "--stdin-filepath" filepath
+          "--parser=css"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-html
+       . ("prettierd" "--stdin-filepath" filepath
+          "--parser=html"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-graphql
+       . ("prettierd" "--stdin-filepath" filepath
+          "--parser=graphql"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-javascript
+       . ("prettierd" "--stdin-filepath" filepath
+          "--parser=babel-flow"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-json
+       . ("prettierd" "--stdin-filepath" filepath
+          "--parser=json"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-json-stringify
+       . ("prettierd" "--stdin-filepath" filepath
+          "--parser=json-stringify"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-markdown
+       . ("prettierd" "--stdin-filepath" filepath
+          "--parser=markdown"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-ruby
+       . ("prettierd" "--stdin-filepath" filepath
+          "--plugin=@prettier/plugin-ruby" "--parser=ruby"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-scss
+       . ("prettierd" "--stdin-filepath" filepath
+          "--parser=scss"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-svelte
+       . ("prettierd" "--stdin-filepath" filepath
+          "--plugin=prettier-plugin-svelte" "--parser=svelte"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-typescript
+       . ("prettierd" "--stdin-filepath" filepath
+          "--parser=typescript"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "=")))
+      (prettier-yaml
+       . ("prettierd" "--stdin-filepath" filepath
+          "--parser=yaml"
+          (string-join (apheleia-formatters-js-indent "--use-tabs" "--tab-width") "="))))
+    apheleia-formatters)))
