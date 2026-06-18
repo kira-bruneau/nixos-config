152911a2c6a8076886abf2f937c906efbfd488e2
Kira Bruneau <kira.bruneau@pm.me>
2026-06-17 20:01:18 -0400

    home/programs/jellyfin-desktop: build without libcec

--- home/programs/jellyfin-desktop/default.nix
+++ home/programs/jellyfin-desktop/default.nix
@@ -1,7 +1,13 @@
 { pkgs, ... }:
 
+let
+  # Build jellyfin-desktop without libcec
+  jellyfin-desktop = pkgs.jellyfin-desktop.overrideAttrs (attrs: {
+    buildInputs = builtins.filter (p: p.pname != "libcec") attrs.buildInputs;
+  });
+in
 {
-  home.packages = with pkgs; [ jellyfin-desktop ];
+  home.packages = [ jellyfin-desktop ];
   wayland.windowManager.sway.config.assigns."4" = [ { app_id = "^org.jellyfin.JellyfinDesktop$"; } ];
   xdg.dataFile."jellyfin-desktop/scripts/mpris.so".source =
     "${pkgs.mpvScripts.mpris}/share/mpv/scripts/mpris.so";
