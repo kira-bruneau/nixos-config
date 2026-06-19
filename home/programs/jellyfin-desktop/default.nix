ed580814b60109de5b4c4acc0904de4c3cbfa08a
Kira Bruneau <kira.bruneau@pm.me>
2026-06-19 18:07:05 -0400

    home/programs/jellyfin-desktop: remove mpris plugin
    MPRIS support is now builtin to jellyfin-desktop

--- home/programs/jellyfin-desktop/default.nix
+++ home/programs/jellyfin-desktop/default.nix
@@ -9,6 +9,4 @@ in
 {
   home.packages = [ jellyfin-desktop ];
   wayland.windowManager.sway.config.assigns."4" = [ { app_id = "^org.jellyfin.JellyfinDesktop$"; } ];
-  xdg.dataFile."jellyfin-desktop/scripts/mpris.so".source =
-    "${pkgs.mpvScripts.mpris}/share/mpv/scripts/mpris.so";
 }
