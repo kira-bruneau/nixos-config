b9fd13d00ac2d6d2b40c511d74e16ca7574a0c7d
Kira Bruneau <kira.bruneau@pm.me>
2026-06-28 11:58:04 -0400

    home/programs/emacs: fix using marksman language server

--- home/programs/emacs/config/major-modes/prog/markdown.el
+++ home/programs/emacs/config/major-modes/prog/markdown.el
@@ -1,3 +1,7 @@
 (use-package markdown-mode
   :custom
-  (markdown-command "pandoc"))
+  (markdown-command "pandoc")
+
+  :init
+  (with-eval-after-load 'lsp-bridge
+    (setopt lsp-bridge-markdown-lsp-server "marksman")))
