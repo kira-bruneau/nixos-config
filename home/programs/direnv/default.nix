3b6381301c85cd97be44cfb909c0e04d2510b50c
Kira Bruneau <kira.bruneau@pm.me>
2026-06-30 09:14:55 -0400

    home/programs/direnv: whitelist owned repos

--- home/programs/direnv/default.nix
+++ home/programs/direnv/default.nix
@@ -2,5 +2,10 @@
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
+    config.whitelist.prefix = [
+      "~/Dev/archive"
+      "~/Dev/private"
+      "~/Dev/public"
+    ];
   };
 }
