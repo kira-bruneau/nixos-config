bc86dbf2d6683ff62051c0a67a999c63c7a01374
Kira Bruneau <kira.bruneau@pm.me>
2026-06-29 21:41:28 -0400

    home/programs/nix-init: use home-manager module

--- home/programs/nix-init/default.nix
+++ home/programs/nix-init/default.nix
@@ -1,15 +1,9 @@
-{ pkgs, ... }:
-
-let
-  settingsFormat = pkgs.formats.toml { };
-in
 {
-  home.packages = with pkgs; [
-    nix-init
-  ];
-
-  xdg.configFile."nix-init/config.toml".source = settingsFormat.generate "config.toml" {
-    maintainers = [ "kira-bruneau" ];
-    commit = true;
+  programs.nix-init = {
+    enable = true;
+    settings = {
+      maintainers = [ "kira-bruneau" ];
+      commit = true;
+    };
   };
 }
