7b711d0be30f00a507677d8497d72db926ee92d3
Kira Bruneau <kira.bruneau@pm.me>
2026-07-20 19:42:18 -0400

    home/programs/emacs: refine behaviour of vertico-directory-enter

--- home/programs/emacs/config/minor-modes/vertico.el
+++ home/programs/emacs/config/minor-modes/vertico.el
@@ -7,26 +7,27 @@
 
 (use-package vertico-directory
   :bind (:map vertico-map
-         ("/" . vertico-directory-insert)
+         ("/" . vertico-directory-enter)
          ("~" . vertico-directory-home)
          ("DEL" . vertico-directory-delete-char)
          ("M-DEL" . vertico-directory-delete-word))
   :hook (rfn-eshadow-update-overlay . vertico-directory-tidy)
   :config
-  (defun vertico-directory-insert (n &optional c)
-    (interactive "p")
-    (if-let (((eq n 1))
-             ((>= vertico--index 0))
-             ((eq (vertico--metadata-get 'category) 'file))
-             ((eq (point) (point-max)))
-             ((not (eq (char-before) ?/)))
-             (pwd (file-name-directory (minibuffer-contents-no-properties)))
-             (cand (vertico--candidate))
-             ((string-prefix-p pwd cand)))
-        (progn
-          (delete-minibuffer-contents)
-          (insert cand))
-      (self-insert-command n c)))
+  (defun vertico-directory-enter (&optional arg c)
+    (interactive "P")
+    (if (and
+         (not arg)
+         (eq (vertico--metadata-get 'category) 'file)
+         (not (string-equal vertico--base ""))
+         (>= vertico--index 0)
+         (eq (point) (point-max)))
+        (let ((cand (vertico--candidate)))
+          (if (file-regular-p cand)
+              (vertico-exit)
+            (progn
+              (delete-minibuffer-contents)
+              (insert cand))))
+      (self-insert-command (prefix-numeric-value arg) c)))
 
   (defun vertico-directory-home ()
     (interactive)
