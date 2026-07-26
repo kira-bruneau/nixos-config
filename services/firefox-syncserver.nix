04931aa1c2ba19671ad1b672d0d6d11486ab80cb
Kira Bruneau <kira.bruneau@pm.me>
2026-07-25 23:24:50 -0400

    services/firefox-syncserver: auto generate sync master secret

--- services/firefox-syncserver.nix
+++ services/firefox-syncserver.nix
@@ -1,4 +1,9 @@
-{ config, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   services.mysql.package = pkgs.mariadb;
@@ -17,7 +22,30 @@
     settings.host = "0.0.0.0";
   };
 
-  systemd.services.firefox-syncserver.serviceConfig.StateDirectory = "firefox-syncserver";
+  systemd.services = {
+    firefox-syncserver.serviceConfig.StateDirectory = "firefox-syncserver";
+
+    firefox-syncserver-pre-setup = {
+      wantedBy = [ "firefox-syncserver.service" ];
+      requires = [ "firefox-syncserver.service" ];
+      before = [ "firefox-syncserver.service" ];
+
+      unitConfig = {
+        ConditionPathExists = "!${config.services.firefox-syncserver.secrets}";
+      };
+
+      serviceConfig = {
+        StateDirectory = "firefox-syncserver";
+        DynamicUser = true;
+        IgnoreSIGPIPE = false; # https://stackoverflow.com/a/44376786
+      };
+
+      script = ''
+        echo "SYNC_MASTER_SECRET=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 64)" \
+          >> ${lib.escapeShellArg config.services.firefox-syncserver.secrets}
+      '';
+    };
+  };
 
   networking.firewall.allowedTCPPorts = [ config.services.firefox-syncserver.settings.port ];
 }
