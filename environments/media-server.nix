1897ebf731d3bf2643f4382ce099f2dea17ba79f
Kira Bruneau <kira.bruneau@pm.me>
2026-08-04 02:25:43 -0400

    environments/media-server: don't disable jellyfin startup wizard
    Startup wizard is needed to seed the database

--- environments/media-server.nix
+++ environments/media-server.nix
@@ -275,7 +275,6 @@ in
             "@xmlns:xsi" = "http://www.w3.org/2001/XMLSchema-instance";
             "@xmlns:xsd" = "http://www.w3.org/2001/XMLSchema";
 
-            IsStartupWizardCompleted = true;
             ServerName = config.networking.hostName;
             UICulture = "en-US";
             PreferredMetadataLanguage = "en";
